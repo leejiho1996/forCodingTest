7097260e260b131c2cfed3a340e118392e8b3b6a
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-03-07 10:40:15 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 48 ms, Memory: 32436 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -5,21 +5,14 @@ input = sys.stdin.readline
 string = input().rstrip()
 n = int(input())
 
-alpha = 'abcdefghijklmnopqrstuvwxyz'
+s = ''
+acc = []
 
-acc = [[0] * (len(string)+1) for _ in range(len(alpha))]
-
-for i in range(len(alpha)):
-    for j in range(len(string)):
-        if alpha[i] == string[j]:
-            acc[i][j] = acc[i][j-1]+1
-        else:
-            acc[i][j] = acc[i][j-1]
+for i in range(len(string)):
+    s += string[i]
+    acc.append(s)
 
 for i in range(n):
-    char, start, end = input().rstrip().split()
-    start = int(start)
-    end = int(end)
-
-    print(acc[alpha.index(char)][end] - acc[alpha.index(char)][start-1])
-            
+    a, start, end = input().rstrip().split()
+    print(string[int(start):int(end)+1].count(a))
+    

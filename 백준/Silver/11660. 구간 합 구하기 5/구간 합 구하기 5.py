31e798fcdc383e070c254dcd5b10ed4d505268f2
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-03-08 10:47:53 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1048 ms, Memory: 105932 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,22 +1,20 @@
-# 구간 합 구하기 5
+# 구간합5 (dp)
 import sys
 input = sys.stdin.readline
 
 n, m = map(int,input().split())
-table = [[0] * (n+1) for _ in range(n)] 
+
+arr = [list(map(int,input().split())) for _ in range(n)]
 
 
-for i in range(n):
-    s = 0
-    for j, n in enumerate(list(map(int,input().split()))):
-        s += n 
-        table[i][j+1] = s
-        
+dp = [[0] * (n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1] + arr[i-1][j-1] - dp[i-1][j-1]
+
 for i in range(m):
-    s_row, s_col, e_row, e_col = map(int,input().split())
+    s_row, s_col, e_row, e_col = map(int, input().split())
 
-    s = 0
-    for j in range(s_row-1, e_row):
-        s += table[j][e_col] - table[j][s_col-1]        
-
-    print(s)
+    result = dp[e_row][e_col] - dp[s_row-1][e_col] - dp[e_row][s_col-1] + dp[s_row -1][s_col-1]
+    print(result)

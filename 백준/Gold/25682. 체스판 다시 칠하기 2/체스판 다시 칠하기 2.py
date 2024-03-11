9b9eb59fb673ace04960a7c77a019b4a3ee2a712
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-03-11 16:26:35 +0900

    [Gold V] Title: 체스판 다시 칠하기 2, Time: 368 ms, Memory: 142488 KB -BaekjoonHub

--- 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.py
+++ 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.py
@@ -5,24 +5,20 @@ input = sys.stdin.readline
 n, m, k = map(int,input().split())
 
 dpB = [[0] * (m+1) for _ in range(n+1)]
-dpW = [[0] * (m+1) for _ in range(n+1)]
 
 for i in range(1, n+1):
     board = input().rstrip()
     for j in range(1, m+1):
         if ((i+j) % 2 == 0 and board[j-1] == "B") or ((i+j) % 2 == 1 and board[j-1] == "W"):
             dpB[i][j] = dpB[i-1][j] + dpB[i][j-1] - dpB[i-1][j-1]
-            dpW[i][j] = dpW[i-1][j] + dpW[i][j-1] - dpW[i-1][j-1] + 1
         else:
             dpB[i][j] = dpB[i-1][j] + dpB[i][j-1] - dpB[i-1][j-1] + 1
-            dpW[i][j] = dpW[i-1][j] + dpW[i][j-1] - dpW[i-1][j-1] 
 
-minn = 10000000000
+minn = 4000001
 
 for i in range(1, n-k+2):
     for j in range(1, m-k+2):
         checkB = dpB[i+k-1][j+k-1] - dpB[i-1][j+k-1] - dpB[i+k-1][j-1] + dpB[i-1][j-1]
-        checkW = dpW[i+k-1][j+k-1] - dpW[i-1][j+k-1] - dpW[i+k-1][j-1] + dpW[i-1][j-1]
-        minn = min(checkB, checkW, minn)
+        minn = min(checkB, k*k - checkB, minn)
 
 print(minn)

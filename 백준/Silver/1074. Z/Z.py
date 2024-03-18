c8e9db8e5616a33a52250acbe9c07019147dc7f7
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-03-18 21:35:51 +0900

    [Silver I] Title: Z, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -8,7 +8,6 @@ base = [[0,1],
         [2,3]]
 
 divide = 2**(n-1)
-multiple = 4**(n-1)
 
 base_num = 0
 
@@ -16,11 +15,11 @@ while divide >= 1:
     n_r = r // divide
     n_c = c // divide
 
-    base_num += multiple * (base[n_r][n_c])
+    base_num += divide**2 * (base[n_r][n_c])
 
     r -= n_r * divide
     c -= n_c * divide
     divide //= 2
-    multiple //= 4
+
     
 print(base_num)

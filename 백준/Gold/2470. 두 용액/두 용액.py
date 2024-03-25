fc0ca83e8eeb4ca110a50b38b77cda9559227a10
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-03-26 01:36:18 +0900

    [Gold V] Title: 두 용액, Time: 128 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -14,18 +14,21 @@ end = n-1
 prev = 2_000_000_001
 min_prev = 2_000_000_001
 
+
 while start < end:
-    if abs(num[end]+num[start]) < prev:
-        prev = abs(num[start]+num[end])
-        if prev < min_prev:
-            min_prev = prev
-            p_start = num[start]
-            p_end = num[end]
-        end -= 1
-        
-    else:
+    s = num[start] + num[end]
+    if abs(s) < prev:
+        prev = abs(s)
+        p_start = num[start]
+        p_end = num[end]
+
+    if s < 0:
         start += 1
-        prev = 2_000_000_001
-        end = min(n-1, end+1)
-                  
+    elif s > 0:
+        end -= 1
+    else:
+        break
+    
 print(p_start, p_end)
+
+# 양수 음수 체크 

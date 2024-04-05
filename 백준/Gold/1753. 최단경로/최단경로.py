2d69d3789f52df52ca6186ed23aa0434afe4de35
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-04-05 18:47:25 +0900

    [Gold IV] Title: 최단경로, Time: 648 ms, Memory: 68336 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,32 +1,32 @@
+# 최단 경로
 import sys
 import heapq
-
 input = sys.stdin.readline
 
-v, e = map(int, input().split())
+v, e = map(int,input().split())
 k = int(input())
 
-# 인접 리스트
-graph = [[] for _ in range(v+1)]
+visit = [[] for _ in range(v+1)]
 
 for _ in range(e):
-    u, t, w = map(int, input().split())
-    graph[u].append((t, w))
+    u, t, w = map(int,input().split())
+
+    visit[u].append((w,t))
 
 def dijkstra(start):
     que = []
     heapq.heappush(que, (0, start))
-    
-    distance = [sys.maxsize] * (v+1)
-    distance[start] = 0
 
+    distance = [sys.maxsize]*(v+1)
+    distance[start] = 0
+    
     while que:
         cur_weight, cur_node = heapq.heappop(que)
 
         if distance[cur_node] < cur_weight:
             continue
         
-        for next_node, next_weight in graph[cur_node]:
+        for next_weight, next_node in visit[cur_node]:
             next_weight += cur_weight
             
             if distance[next_node] <= next_weight:
@@ -34,12 +34,13 @@ def dijkstra(start):
             
             distance[next_node] = next_weight
             heapq.heappush(que, (next_weight, next_node))
-
+            
+            
     return distance
 
 distance = dijkstra(k)
 
-for i in range(1, v+1):
+for i in range(1,v+1):
     if distance[i] == sys.maxsize:
         print("INF")
     else:

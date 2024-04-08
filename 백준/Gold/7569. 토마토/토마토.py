424ecf58b7c7ba5769a812486c50a928c1627de2
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-04-09 08:50:42 +0900

    [Gold V] Title: 토마토, Time: 2088 ms, Memory: 52524 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,45 +1,60 @@
-# 토마토(3d)
+#7569
 import sys
 from collections import deque
+all_fire_tomato = True
 input = sys.stdin.readline
+q = deque()
+M, N, H = map(int, input().split())
+result = 0
+arr = []
+for i in range(H):
+    temp = []
+    for j in range(N):
+        zxc = list(map(int, input().split()))
+        count = 0
+        for num in zxc:
+            if num == 0:
+                all_fire_tomato = False
+            elif num == 1:
+                q.append((i,j,count))
+            count +=1
+        temp.append(zxc)
+    arr.append(temp)
 
-m, n, h = map(int,input().split())
 
-board = [[list(map(int,input().split())) for _ in range(n)] for _ in range(h)]
+# 왼 오 앞 뒤 위 아래
+dx = [-1, 1, 0, 0, 0, 0]
+dy = [0, 0, -1, 1, 0, 0]
+dz = [0, 0, 0, 0, -1, 1]
 
-pos = [(0,-1,0), (0,1,0), (0,0,-1), (0,0,1), (1,0,0),(-1,0,0)]
+def bfs():
+    global result
 
-target = m*n*h
-que = deque([])
 
-for k in range(h):
-    for i in range(n):
-        for j in range(m):
-            if board[k][i][j] == 1:
-                que.append((k,i,j,0))
-            if board[k][i][j] == -1:
-                target -= 1
+    while q:
+        c, b, a = q.popleft()
 
-cnt = len(que)
+        for i in range(6):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            nz = c + dz[i]
 
-if cnt == target:
+            if 0 <= nx < M and 0 <= ny < N and 0 <= nz < H:
+                if arr[nz][ny][nx] == 0:
+                    arr[nz][ny][nx] = arr[c][b][a] + 1
+                    temp = [nz, ny, nx]
+                    q.append(temp)
+                    if result < arr[nz][ny][nx]:
+                        result= arr[nz][ny][nx]
+
+if all_fire_tomato:
     print(0)
-    exit()
-
-while que:
-    height, row, col, day = que.popleft()
-
-    for he, r, c in pos:
-        new_height = height + he
-        new_row = row + r
-        new_col = col + c
-
-        if 0 <= new_height < h and 0 <= new_row < n and 0 <= new_col < m and board[new_height][new_row][new_col] == 0:
-            que.append((new_height, new_row, new_col, day+1))
-            cnt += 1
-            board[new_height][new_row][new_col] = -1
-            if cnt == target:
-                print(day+1)
-                exit()
-    
-print(-1)
+else:
+    bfs()
+    for k in range(H):
+        for i in range(N):
+            for j in range(M):
+                if arr[k][i][j] == 0:
+                    print(-1)
+                    sys.exit()
+    print(result-1)

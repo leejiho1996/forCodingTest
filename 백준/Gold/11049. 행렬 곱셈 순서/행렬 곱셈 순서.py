d3c6db79741412ba755829d2a0553e40d4d8c600
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-04-11 01:11:20 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 808 ms, Memory: 112572 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -3,20 +3,24 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-limit = pow(2, 30) - 1
-dp = [[limit]*n for _ in range(n)]
-
 matrix = [list(map(int,input().split())) for _ in range(n)]
+
+limit = pow(2, 30) - 1
+dp = [[limit] * n for _ in range(n)]
 
 for i in range(n):
     dp[i][i] = 0
+    
+def sequence():
+    for i in range(1, n):
+        for j in range(n-i):
+            # start와 end 정하기
+            start = j
+            end = i + j
+            for mid in range(start, end):
+                dp[start][end] = min(dp[start][end],
+                                     dp[start][mid] + dp[mid+1][end] # start에서 mid까지, mid에서 start까지 합
+                                     + matrix[start][0]*matrix[mid+1][0]*matrix[end][1]) # 합쳐지는 부분의 행렬곱
+    print(dp[0][n-1])
 
-for k in range(1, n):
-    for i in range(n-k):
-        for j in range(i, i + k):
-            s, m, e = i, j, i+k
-            # print(s,m,e)
-            dp[s][e] = min(dp[s][e], dp[s][m] + dp[m+1][e] + matrix[s][0] * matrix[m][1] * matrix[e][1])
-
-
-print(dp[0][n-1])
+sequence()

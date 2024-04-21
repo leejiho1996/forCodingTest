da23fddfa7ea0e48e06c43918b188b1249b0f458
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-04-21 15:15:21 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 764 ms, Memory: 50548 KB -BaekjoonHub

---
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.py
@@ -0,0 +1,44 @@
+# 오아시스 재결합
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+limit = float('inf')
+num = []
+
+for i in range(n):
+    num.append(int(input()))
+
+num.append(limit)
+
+stack = [(num[0], 0, 0)]
+cnt = 0
+
+for i in range(1, n+1):
+    # print("stack", stack, "cnt", cnt, "num", num[i])
+    total = 0
+    cur = num[i]
+    sa = 0
+    maxx = 0
+    
+    while stack and stack[-1][0] <= num[i]:
+        number, r, same = stack.pop()
+
+        if maxx <= number:
+            total += 1
+            
+        if number == num[i]:
+            sa = (same + 1)
+
+        total += same
+
+        cnt += r
+        
+        maxx = max(number, maxx)
+
+    if stack:
+        total += 1
+            
+    stack.append((num[i], total, sa))
+
+print(cnt)

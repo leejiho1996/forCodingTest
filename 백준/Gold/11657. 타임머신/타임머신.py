82c01a235eccfc92198ebb6ca7d5e2061de2f881
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-04-22 09:09:09 +0900

    [Gold IV] Title: 타임머신, Time: 1388 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -0,0 +1,33 @@
+# 타임 머신
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+
+route = []
+
+for i in range(m):
+    a, b, c = map(int,input().split())
+    route.append((a-1, b-1, c))
+
+visit = [float("inf")] * n
+visit[0] = 0
+check = False
+
+for i in range(n-1):
+    for start, to, cost in route:
+        if visit[start] != float('inf') and visit[start] + cost < visit[to]:
+            visit[to] = visit[start] + cost
+
+for start, to, cost in route:
+    if visit[start] != float('inf') and visit[start] + cost < visit[to]:
+        check = True
+        print(-1)
+        break
+
+if not check:
+    for i in range(1, n):
+        if visit[i] == float('inf'):
+            print(-1)
+        else:
+            print(visit[i])

35d95cf4b8213ae72aac5c6bf439a85d1d7e5ada
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-04-22 09:59:39 +0900

    [Gold IV] Title: 타임머신, Time: 712 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -10,24 +10,28 @@ for i in range(m):
     a, b, c = map(int,input().split())
     route.append((a-1, b-1, c))
 
-visit = [float("inf")] * n
-visit[0] = 0
-check = False
 
-for i in range(n-1):
+def bell():
+    check = False
+    
+    visit = [float("inf")] * n
+    visit[0] = 0
+    
+    for i in range(n-1):
+        for start, to, cost in route:
+            if visit[start] != float('inf') and visit[start] + cost < visit[to]:
+                visit[to] = visit[start] + cost
+
     for start, to, cost in route:
         if visit[start] != float('inf') and visit[start] + cost < visit[to]:
-            visit[to] = visit[start] + cost
-
-for start, to, cost in route:
-    if visit[start] != float('inf') and visit[start] + cost < visit[to]:
-        check = True
-        print(-1)
-        break
-
-if not check:
-    for i in range(1, n):
-        if visit[i] == float('inf'):
+            check = True
             print(-1)
-        else:
-            print(visit[i])
+            break
+
+    if not check:
+        for i in range(1, n):
+            if visit[i] == float('inf'):
+                print(-1)
+            else:
+                print(visit[i])
+bell()

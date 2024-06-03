e09669f1759f87cc11ded5f7df9f3c4128b126f6
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-06-04 00:52:09 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 416 ms, Memory: 49332 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,39 @@
+# 최소 스패닝 트리
+import sys
+input = sys.stdin.readline
+import heapq
+sys.setrecursionlimit(100001)
+
+v, e = map(int,input().split())
+
+check = [i for i in range(0, v+1)]
+
+def find(n):
+    if check[n] != n:
+        check[n] = find(check[n])
+    return check[n]
+    
+que = []
+total = 0
+
+for i in range(e):
+    start, to, cost = map(int,input().split())
+    heapq.heappush(que,(cost, start, to))
+
+while que:
+    cost, start, to = heapq.heappop(que)
+
+    start_root = find(start)
+    to_root = find(to)
+
+    if start_root == to_root:
+        continue
+    else:
+        if start > to:
+            check[start_root] = to_root
+        else:
+            check[to_root] = start_root
+
+        total += cost
+
+print(total)

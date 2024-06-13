dfb30141f27cd28dde09ec188228e552df300dc9
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-06-13 17:31:32 +0900

    [Gold II] Title: 플로이드 2, Time: 472 ms, Memory: 33164 KB -BaekjoonHub

---
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+graph = [[float('inf')] * (n + 1) for _ in range(n + 1)]
+route = [[[] for _ in range(n + 1)] for __ in range(n + 1)]
+
+for i in range(1, n + 1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    start, to, cost = map(int, input().split())
+    if graph[start][to] > cost:
+        graph[start][to] = cost
+        route[start][to] = [start, to]
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if graph[i][j] > graph[i][k] + graph[k][j]:
+                graph[i][j] = graph[i][k] + graph[k][j]
+                route[i][j] = route[i][k][:-1] + route[k][j]
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if graph[i][j] == float('inf'):
+            print(0, end=' ')
+        else:
+            print(graph[i][j], end=' ')
+    print()
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if len(route[i][j]) == 0:
+            print(0)
+        else:
+            print(len(route[i][j]), end=' ')
+            print(*route[i][j])

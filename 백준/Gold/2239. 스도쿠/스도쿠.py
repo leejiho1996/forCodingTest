4f75acc1d41744180b82c1ace37d60a81d05fc61
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-06-28 14:36:23 +0900

    [Gold IV] Title: 스도쿠, Time: 2300 ms, Memory: 119396 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -4,28 +4,20 @@ input = sys.stdin.readline
 board = []
 col = [[] for _ in range(9)]
 zero = []
+three = [[0] * 10 for _ in range(9)] 
 
-cnt = 0
 for i in range(9):
     board.append(list(input().rstrip()))
     for j in range(9):
         col[j].append(board[i][j])
         if board[i][j] == '0':
-            cnt += 1
             zero.append((i, j))
+        three_idx = (i // 3 * 3) + (j // 3)
+
+        three[three_idx][int(board[i][j])] = 1
 
 limit = len(zero)
 
-def three(i ,j):
-    row = i // 3 * 3
-    col = j // 3 * 3
-    three_list = []
-
-    for i in range(row, row+3):
-        for j in range(col, col+3):
-            if board[i][j] != 0:
-                three_list.append(board[i][j]) 
-    return three_list
 
 def backtrack(idx):
     if idx == limit:
@@ -34,14 +26,19 @@ def backtrack(idx):
         exit()
     
     x, y = zero[idx]
-    three_list = three(x, y)
+    three_idx = (x // 3 * 3) + (y // 3)
+
 
     for i in range(1, 10):
-        if str(i) not in board[x] and str(i) not in col[y] and str(i) not in three_list:
+        if str(i) not in board[x] and str(i) not in col[y] and str(i) and three[three_idx][i] != 1:
             col[y][x] = str(i)
             board[x][y] = str(i)
+            three[three_idx][i] = 1
             backtrack(idx+1)
             board[x][y] = '0'
             col[y][x] = '0'
+            three[three_idx][i] = 0
 
 backtrack(0)
+
+

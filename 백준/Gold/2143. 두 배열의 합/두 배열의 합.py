082ef2e4ffbb42489f3a394badb4b8113782f9d9
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-07-01 15:37:52 +0900

    [Gold III] Title: 두 배열의 합, Time: 340 ms, Memory: 88612 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
@@ -9,7 +9,7 @@ n_part = []
 
 m = int(input())
 m_num = list(map(int,input().split()))
-m_part = []
+
 
 for i in range(n):
     n_part.append(n_num[i])
@@ -17,27 +17,22 @@ for i in range(n):
     for j in range(i+1, n):
         total += n_num[j]
         n_part.append(total)
-n_part.sort()
 
 dic = {}
 
 for i in range(m):
-    m_part.append(m_num[i])
     total = m_num[i]
     if m_num[i] in dic:
         dic[m_num[i]] += 1
     else:
         dic[m_num[i]] = 1
-        
+
     for j in range(i+1, m):
         total += m_num[j]
         if total in dic:
             dic[total] += 1
         else:
             dic[total] = 1
-        m_part.append(total)
-
-m_part.sort()
 
 cnt= 0
 for i in n_part:

b2b9dbc0121e8798d3d1d4f1724e8ef174ed0d85
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-07-31 22:16:02 +0900

    [Platinum IV] Title: 경찰차, Time: 1004 ms, Memory: 166092 KB -BaekjoonHub

--- 백준/Platinum/2618. 경찰차/경찰차.py
+++ 백준/Platinum/2618. 경찰차/경찰차.py
@@ -1,84 +1,83 @@
-# 경찰차
+# 경찰차 이중 for
+def distance(x1, y1, x2, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def solve(n, w, events):
+    dp = [[float('inf')] * (w + 1) for _ in range(w + 1)]
+    dp[0][0] = 0
+    trace = [[(-1, -1)] * (w + 1) for _ in range(w + 1)]
+    
+    for i in range(w + 1):
+        for j in range(w + 1):
+            next_event = max(i, j) + 1
+            if next_event > w:
+                continue
+            
+            event_pos = events[next_event]
+            if i == 0:
+                car1_pos = (1, 1)
+            else:
+                car1_pos = events[i]
+            if j == 0:
+                car2_pos = (n, n)
+            else:
+                car2_pos = events[j]
+
+            new_dist_car1 = dp[i][j] + distance(car1_pos[0], car1_pos[1], event_pos[0], event_pos[1])
+            new_dist_car2 = dp[i][j] + distance(car2_pos[0], car2_pos[1], event_pos[0], event_pos[1])
+            
+            if new_dist_car1 < dp[next_event][j]:
+                dp[next_event][j] = new_dist_car1
+                trace[next_event][j] = (i, j)
+            if new_dist_car2 < dp[i][next_event]:
+                dp[i][next_event] = new_dist_car2
+                trace[i][next_event] = (i, j)
+    
+    result = float('inf')
+    last_i, last_j = -1, -1
+    for i in range(w + 1):
+        if dp[w][i] < result:
+            result = dp[w][i]
+            last_i, last_j = w, i
+        if dp[i][w] < result:
+            result = dp[i][w]
+            last_i, last_j = i, w
+    
+    # 경로 추적
+    path = []
+    x, y = last_i, last_j
+    while trace[x][y] != (-1, -1):
+        px, py = trace[x][y]
+        if px == x:
+            path.append(2)
+        else:
+            path.append(1)
+        x, y = px, py
+    
+    path.reverse()
+    return result, path
+
+# 입력 처리
 import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10000001)
+input = sys.stdin.read
+data = input().strip().split()
+index = 0
 
-n = int(input()) # N*N 도로의 갯수
-w = int(input()) # 사건의 갯수
+n = int(data[index])
+index += 1
+w = int(data[index])
+index += 1
 
-w_list = [(0,0)]
-seq = []
+events = [(0, 0)] * (w + 1)
+for i in range(1, w + 1):
+    x = int(data[index])
+    index += 1
+    y = int(data[index])
+    index += 1
+    events[i] = (x, y)
 
-for i in range(w):
-    a, b = map(int,input().split())
-    w_list.append((a,b))
-
-# 거리계산 함수
-def cal_distance(x1, y1, x2, y2):
-    x = abs(x1-x2)
-    y = abs(y1-y2)
-    return x + y
-
-def cal_dp(i, j):
-    
-    if i == w or j == w: # 사건 갯수 w에 도달했다면 0을 리턴
-        return 0
-
-    if dp[i][j] != -1:
-        return dp[i][j]
-
-    # 다음 사건 
-    next_w = max(i, j) + 1
-  
-    if i == 0:
-        next_p1 = cal_distance(1, 1, w_list[next_w][0], w_list[next_w][1])
-    else:
-        next_p1 = cal_distance(w_list[i][0], w_list[i][1], w_list[next_w][0], w_list[next_w][1])
-
-    if j == 0:
-        next_p2 = cal_distance(n, n, w_list[next_w][0], w_list[next_w][1])
-    else:
-        next_p2 = cal_distance(w_list[j][0], w_list[j][1], w_list[next_w][0], w_list[next_w][1])
-
-    dp[i][j] = min(cal_dp(next_w, j) + next_p1, cal_dp(i, next_w) + next_p2)
-    
-    return dp[i][j]
-
-def cal_seq(i, j):
-    if i == w or j == w:
-        return
-
-    next_w = max(i, j) + 1
-
-    if i == 0:
-        p1 = cal_distance(1, 1, w_list[next_w][0], w_list[next_w][1])
-    else:
-        p1 = cal_distance(w_list[i][0], w_list[i][1], w_list[next_w][0], w_list[next_w][1])
-
-    if j == 0:
-        p2 = cal_distance(n, n, w_list[next_w][0], w_list[next_w][1])
-    else:
-        p2 = cal_distance(w_list[j][0], w_list[j][1], w_list[next_w][0], w_list[next_w][1])
-
-    if dp[next_w][j] + p1 > dp[i][next_w] + p2:
-        print(2)
-        cal_seq(i, next_w)
-    else:
-        print(1)
-        cal_seq(next_w, j)
-
-# 경찰차 좌표
-p1 = [1,1]
-p2 = [n, n]
-
-# 최소거리dp
-# dp[i][j]는 1번째 경찰차가 마지막으로 i번째 사건을 방문 2번째 경찰차가 마지막으로 j번째 사건을 방문 했을 때 남은 최소 거리 수
-dp = [[-1]*(w+1) for _ in  range(w+1)]
-
-# 사건이 맡겨진 경찰차 번호
-p_num = [0] * (w)
-
-
-
-print(cal_dp(0,0))
-cal_seq(0,0)
+# 문제 해결 및 결과 출력
+result, path = solve(n, w, events)
+print(result)
+for p in path:
+    print(p)

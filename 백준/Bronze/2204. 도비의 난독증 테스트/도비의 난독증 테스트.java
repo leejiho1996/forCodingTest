24448d5a9c015d6615d9040e4cedd46b9906f56d
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-08-12 01:47:22 +0900

    [Bronze I] Title: 도비의 난독증 테스트, Time: 192 ms, Memory: 18100 KB -BaekjoonHub

---
+++ 백준/Bronze/2204. 도비의 난독증 테스트/도비의 난독증 테스트.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        HashMap<String, String> hashMap = new HashMap<>();
+
+        while (input != 0) {
+            String[] arr = new String[input];
+            for (int i = 0; i < input; i++) {
+                arr[i] = sc.next();
+            }
+            for (int i = 0; i < arr.length; i++) {
+                String temp = arr[i].toLowerCase();
+                hashMap.put(temp, arr[i]);
+                arr[i] = arr[i].toLowerCase();
+            }
+            Arrays.sort(arr);
+            System.out.println(hashMap.get(arr[0]));
+            input = sc.nextInt();
+        }
+    }
+}
+

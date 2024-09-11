a0b2a5964e1e14527dd96461f5f37f461d660d99
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-09-12 01:33:40 +0900

    [Gold IV] Title: 가르침, Time: 672 ms, Memory: 112648 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.py
+++ 백준/Gold/1062. 가르침/가르침.py
@@ -8,46 +8,49 @@ if k < 5:
     print(0)
     exit()
 
-base = {"a", "n", "t", "c", "i"}
+base = ["a", "n", "t", "c", "i"]
+word_list = []
+is_used = [False] * 26
 
-alphabet = {}
-
-for i in range(26):
-    alphabet[chr(97+i)] = 0
-
-word_set = []
-candi = []
-
-def dfs(w, depth):
-    
-    next_chr = chr(96+depth)
-    
-    if len(w) == k - 5:
-        candi.append(w)
-        return
-    
-    if (26 - depth+1) + len(w) < k - 5: # (26-depth) -> 남은 알파벳 
-        return
-
-    dfs(w, depth+1)
-
-    if next_chr not in base:    
-        dfs(w+next_chr, depth+1)
-    
-
-for i in range(n):
-    word = set(input().rstrip())
-    word_set.append(word-base)
-
-dfs("", 1)
+for i in base:
+    is_used[ord(i) - 97] = True
 
 maxx = 0
-for i in range(len(candi)):
-    check = set(candi[i])
-    cnt = 0
-    for j in range(n):
-        if len(word_set[j].difference(check)) == 0:
-            cnt += 1        
-    maxx = max(maxx, cnt)
+def dfs(start, length):
+
+    global maxx
+    
+    if length == k - 5:
+      
+        cnt = 0 
+        for i in range(n):
+            word = word_list[i]
+            possible = True
+
+            for j in word:
+                if not is_used[ord(j) - 97]:
+                    possible = False
+                    break
+
+            if possible:
+                cnt += 1
+                
+        maxx = max(maxx, cnt)
+        return
+    
+    for i in range(start, 32 - k + length): # k 는 5 이상
+        
+        if not is_used[i]:
+            is_used[i] = True
+            dfs(i, length+1)
+            is_used[i] = False
+        
+for i in range(n):
+    word = input().rstrip()
+    word = word.replace("anta", "")
+    word = word.replace("tica", "")
+    word_list.append(word)
+
+dfs(0, 0)
 
 print(maxx)

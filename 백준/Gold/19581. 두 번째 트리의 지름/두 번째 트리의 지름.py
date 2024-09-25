40c4f439d07ddbdac67da57fa18b0005261bc5a7
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-09-25 20:44:10 +0900

    [Gold I] Title: 두 번째 트리의 지름, Time: 660 ms, Memory: 85916 KB -BaekjoonHub

--- 백준/Gold/19581. 두 번째 트리의 지름/두 번째 트리의 지름.py
+++ 백준/Gold/19581. 두 번째 트리의 지름/두 번째 트리의 지름.py
@@ -12,56 +12,39 @@ for i in range(n-1):
     tree[start].append((to, cost))
     tree[to].append((start, cost))
 
-visited = [-1] * (n+1)
-
 max_dist = 0
 max_node = 0
 
-def dfs(node, dist, oppo = 0):
+def dfs(node, prev, dist, oppo = 0):
     global max_dist
     global max_node
-
-    visited[node] = 1
     
     for i in tree[node]:
         to, cost = i
 
-        if to == oppo:
+        if to == oppo or to == prev:
             continue
-
-        if visited[to] != -1:
-            continue
-        else:
-            visited[to] = 1
 
         if max_dist < cost + dist:
             max_dist = cost + dist
             max_node = to
 
-        dfs(to, cost+dist, oppo)
+        dfs(to, node, cost+dist, oppo)
 
-dfs(1, 0)
+dfs(1, 0, 0)
 first_node = max_node
 
 max_dist = 0
-max_node = 0
-visited = [-1] * (n+1)
-dfs(first_node, 0)
+dfs(first_node, 0, 0)
 second_node = max_node
 
 max_dist = 0
-max_node = 0
-visited = [-1] * (n+1)
-dfs(first_node, 0, second_node)
+dfs(first_node, 0, 0, second_node)
 first_max = max_dist
-first_second_node = max_node
 
 max_dist = 0
-max_node = 0
-visited = [-1] * (n+1)
-dfs(second_node, 0, first_node)
+dfs(second_node, 0, 0, first_node)
 second_max = max_dist
-second_second_node = max_node
 
 if first_max > second_max:
     print(first_max)

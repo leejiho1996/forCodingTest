2a89aaface0669deaf591a757b7817c9ff163050
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-10-15 18:19:40 +0900

    [Gold IV] Title: 소트, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1083. 소트/소트.py
+++ 백준/Gold/1083. 소트/소트.py
@@ -13,25 +13,14 @@ def swap(idx, target):
         elements[i-1] = tmp
         
 for i in range(n):
-    cur = elements[i]
-    max_num = 0
-    max_idx = -1
-    max_move = 0
-    for j in range(i+1, n):
-        ch_num = elements[j]
-        idx = j
-        move = idx - i
+    end = min(i+s, n-1)
+    start = i
 
-        if ch_num <= cur or move > s:
-            continue
-        
-        if ch_num > max_num:
-            max_num = ch_num
-            max_idx = idx
-            max_move = move
+    for j in range(i+1, end+1):
+        if elements[start] < elements[j]:
+            start = j
 
-    if max_idx != -1:
-        s -= max_move
-        swap(max_idx, i)
+    s -= (start-i)
+    swap(start, i)
         
 print(*elements)

852de5d32d3e98354878edabb4cb35cbd226c7ab
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-10-17 02:23:05 +0900

    [Gold IV] Title: 알파벳, Time: 4276 ms, Memory: 168644 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -10,8 +10,9 @@ for i in range(r):
 
 maxx = 0
 direc = [(0,1), (0,-1), (1,0), (-1,0)]
+visited = [0] * 26
 
-def dfs(x, y, cnt, alphabet):
+def dfs(x, y, cnt):
     global maxx
 
     maxx = max(maxx, cnt)
@@ -19,15 +20,20 @@ def dfs(x, y, cnt, alphabet):
     for i in direc:
         new_x = x + i[0]
         new_y = y +i[1]
-
-        if not(0 <= new_x < r) or not(0 <= new_y < c) or graph[new_x][new_y] in alphabet:
+        
+        if not(0 <= new_x < r) or not(0 <= new_y < c):
             continue
 
-        new_alphabet = alphabet+graph[new_x][new_y]
-    
-        dfs(new_x, new_y, cnt+1, new_alphabet)
+        char = ord(graph[new_x][new_y]) - ord("A")
 
-start = graph[0][0]
+        if visited[char]:
+            continue
 
-dfs(0, 0, 1, start)
+        visited[char] = 1
+        dfs(new_x, new_y, cnt+1)
+        visited[char] = 0
+        
+start = ord(graph[0][0]) - ord("A")
+visited[start] = 1
+dfs(0, 0, 1)
 print(maxx) 

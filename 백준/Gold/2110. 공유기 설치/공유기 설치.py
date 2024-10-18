4720b26646011ae931ac1b1b6352975579e92b5c
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-10-18 19:25:05 +0900

    [Gold IV] Title: 공유기 설치, Time: 424 ms, Memory: 38848 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -11,8 +11,8 @@ if c == 2:
     print(house[n-1] - house[0])
     exit()
 
-start = 1
-end = house[n-1] - house[0]
+start = 1 # 최소거리 1
+end = house[n-1] - house[0] # 최대거리
 
 while start <= end:
     mid = (start + end) // 2
@@ -25,6 +25,8 @@ while start <= end:
             cnt += 1
             now = house[i]
 
+    # Lowerbound로는 풀 수 없다. ex) 거리가 2 인경우에도 cnt는 3 이나옴
+    # UpperBound를 구하고 -1 해서 구해야한다.
     if cnt >= c:
         start = mid + 1
     else:

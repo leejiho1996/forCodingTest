4655e77eb0cdf06d305e3aca412e29d5c318019c
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-10-25 22:53:05 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 2706.56 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,21 +1,53 @@
 def solution(cap, n, deliveries, pickups):
     answer = 0
+    total_deliveries = sum(deliveries)
+    total_pickups = sum(pickups)
+    
+    last_delivery_index = n - 1
+    last_pickup_index = n - 1
+    
+    # 가장 마지막 배달/수거 위치 찾기
+    while last_delivery_index >= 0 and deliveries[last_delivery_index] == 0:
+        last_delivery_index -= 1
+    while last_pickup_index >= 0 and pickups[last_pickup_index] == 0:
+        last_pickup_index -= 1
+    
+    while total_deliveries > 0 or total_pickups > 0:
+        # 이번 왕복에서 배달/수거할 수 있는 최대량 설정
+        delivery_capacity = cap
+        pickup_capacity = cap
 
-    d = 0
-    p = 0
+        # 왕복 거리 계산
+        answer += (max(last_delivery_index, last_pickup_index) + 1) * 2
+        
+        # 배달 작업
+        for i in range(last_delivery_index, -1, -1):
+            if deliveries[i] <= delivery_capacity:
+                delivery_capacity -= deliveries[i]
+                total_deliveries -= deliveries[i]
+                deliveries[i] = 0
+            else:
+                deliveries[i] -= delivery_capacity
+                total_deliveries -= delivery_capacity
+                delivery_capacity = 0
+                last_delivery_index = i
+                break
+        else:
+            last_delivery_index = -1
 
-    for i in range(n-1, -1, -1):
-
-        cnt = 0
-
-        d -= deliveries[i]
-        p -= pickups[i]
-
-        while d < 0 or p < 0:
-            d += cap
-            p += cap
-            cnt += 1
-
-        answer += (i + 1) * 2 * cnt
+        # 수거 작업
+        for i in range(last_pickup_index, -1, -1):
+            if pickups[i] <= pickup_capacity:
+                pickup_capacity -= pickups[i]
+                total_pickups -= pickups[i]
+                pickups[i] = 0
+            else:
+                pickups[i] -= pickup_capacity
+                total_pickups -= pickup_capacity
+                pickup_capacity = 0
+                last_pickup_index = i
+                break
+        else:
+            last_pickup_index = -1
 
     return answer

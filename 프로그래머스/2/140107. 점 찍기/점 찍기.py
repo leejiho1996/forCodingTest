de2b8a51f6ce5c04f0a5af78a6a5f4c439706bb9
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-10-30 20:12:05 +0900

    [level 2] Title: 점 찍기, Time: 533.58 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.py
@@ -0,0 +1,9 @@
+def solution(k, d):
+    answer = 0
+    
+    check = d**2
+    for i in range(0, d+1, k):
+        possible = int((check - i**2)**(0.5))
+        answer += possible // k + 1
+        
+    return answer

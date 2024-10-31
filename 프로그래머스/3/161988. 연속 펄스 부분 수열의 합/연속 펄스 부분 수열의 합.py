2b873ebd8e6b01727eca340a073bdb37317e00ef
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-11-01 02:03:00 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 355.12 ms, Memory: 67.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -0,0 +1,31 @@
+def solution(sequence):
+    
+    start_plus = sequence.copy()
+    start_minus = sequence.copy()
+    length = len(sequence)
+    mul = 1
+    
+    for i in range(length):
+        start_plus[i] *= mul
+        start_minus[i] *= -mul
+        mul = -mul
+        
+    plus = 0
+    minus = 0
+    max_plus = -float('inf')
+    max_minus = -float('inf')
+    
+    for i in range(length):
+        plus += start_plus[i]
+        max_plus = max(max_plus, plus)
+        if plus < 0:
+            plus = 0
+        
+        minus += start_minus[i]
+        max_minus = max(max_minus, minus)
+        if minus < 0:
+            minus = 0
+        
+            
+    
+    return max(max_plus, max_minus)

b57efc74d3ec4773d1940e8f3e9399842077a8eb
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-11-03 22:41:15 +0900

    [Gold II] Title: 로봇 조종하기, Time: 592 ms, Memory: 88976 KB -BaekjoonHub

---
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] graph = new int[n][m];
+        int[][] dp = new int[n][m];
+
+        for (int i = 0; i < n ; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp[0][0] = graph[0][0];
+        // 첫행 dp 채우기
+        for (int i = 1; i < m; i++) {
+            dp[0][i] = dp[0][i-1] + graph[0][i];
+        }
+
+        for (int i = 1; i < n; i++) {
+
+            int[] right = Arrays.copyOf(graph[i], m);
+            int[] left = Arrays.copyOf(graph[i], m);
+
+            // 오른쪽 방향 진행
+            for (int j = 0 ; j < m ; j++) {
+                if (j == 0) {
+                    right[j] = dp[i-1][j] + graph[i][j];
+                    continue;
+                }
+                right[j] = Math.max(dp[i-1][j] + graph[i][j], right[j-1] + graph[i][j]);
+            }
+
+            // 왼쪽 방향 진행
+            for (int j = m-1; j >= 0; j--) {
+                if (j == m-1) {
+                    left[j] = dp[i-1][j] + graph[i][j];
+                    continue;
+                }
+                left[j] =  Math.max(dp[i-1][j] + graph[i][j], left[j+1] + graph[i][j]);
+            }
+
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = Math.max(right[j], left[j]);
+            }
+        }
+
+        System.out.println(dp[n-1][m-1]);
+
+    }
+}

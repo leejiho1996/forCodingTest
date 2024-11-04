adaf1d285b0e63f35acaec688d5cb59fde05fa94
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-11-04 23:14:22 +0900

    [Gold I] Title: 횡단보도, Time: 3004 ms, Memory: 255872 KB -BaekjoonHub

---
+++ 백준/Gold/24042. 횡단보도/횡단보도.py
@@ -0,0 +1,51 @@
+# 횡단보도
+import sys
+input = sys.stdin.readline
+import heapq as hq
+import math
+
+n, m = map(int,input().split())
+
+graph = [[] for _ in range(n+1)]
+
+for i in range(m):
+    s, e = map(int,input().split())
+
+    graph[s].append((i, e))
+    graph[e].append((i, s))
+
+visited = [0] * (n+1)
+visited[1] = 1
+min_distance = [float('inf')] * (n+1)
+heap = []
+
+for time, to in graph[1]:
+    hq.heappush(heap, (time+1, to))
+
+while heap:
+    time, cur = hq.heappop(heap)
+    
+    if cur == n:
+        print(time)
+        break
+
+    if visited[cur]:
+        continue
+
+    visited[cur] = time
+    
+    for nt, nl in graph[cur]:
+        if visited[nl]:
+            continue
+        
+        if nt > time: # 건널수 있다면 다음 시간은+1 
+            next_time = nt + 1
+        else:
+            next_time = nt + math.ceil((time - nt) / m) * m + 1 
+
+        if min_distance[nl] < next_time:
+            continue
+        else:
+            min_distance[nl] = next_time
+            hq.heappush(heap, (next_time, nl))
+    

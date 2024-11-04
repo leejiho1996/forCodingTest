6e98cf4fbd2512b87cfec2b4ff69a94999f19dd1
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-11-04 17:49:51 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 2016.72 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,27 +1,57 @@
-def dfs(cnt, users, emoticons, sale_percent):
-    if cnt == len(emoticons):
-        total = 0
-        sub = 0
-        for dis, lim in users:
-            part = 0
-            for j in range(len(emoticons)):
-                if sale_percent[j] < dis:
-                    continue
-                part += emoticons[j] * (100 - sale_percent[j]) // 100
-            if part >= lim:
-                sub += 1
-            else:
-                total += part           
-        return [sub, total]
-    
-    lis = []
-    for i in [10, 20, 30, 40]:
-        sale_percent[cnt] = i
-        lis.append(dfs(cnt+1, users, emoticons, sale_percent))
-        
-    lis.sort(key = lambda x:(-x[0], -x[1]))
-    return lis[0]
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+def make_percentage_cases(prev):
+    cases = []
+    for li in prev:
+        for n in [40, 30, 20, 10]:
+            cases.append(li + [n])
+    return cases
 
 def solution(users, emoticons):
-    answer = dfs(0, users, emoticons, [0] * len(emoticons))
-    return answer
+    answer = []
+
+    cases = [[]]  # 가능한 이모티콘별 할인율 케이스들
+    for _ in range(len(emoticons)):
+        cases = make_percentage_cases(cases)
+
+    for case in cases:  # 완전 탐색
+        result = [0, 0]
+        for percentage, price in users:
+            cost = 0
+            for i in range(len(emoticons)):
+                if case[i] >= percentage:  # 희망 할인율 이상이라면 구매
+                    cost += emoticons[i] * (100 - case[i]) // 100
+            if cost >= price:
+                result[0] += 1
+            else:
+                result[1] += cost
+        answer.append(result)
+    answer.sort(key=lambda x:(-x[0], -x[1]))
+    return answer[0]

16cbeb04aadb56949a2d6eec3768da2b6e0a8c99
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-11-11 17:57:03 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 130.88 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -1,50 +1,53 @@
-def yaksoo(num):
-    yak = set()
-    
-    for i in range(2, int(num**(0.5))+1):
-        if num % i == 0:
-            yak.add(i)
-            yak.add(num // i)
-    yak.add(num)
-    
-    return yak
 
-def cal_max(pos, A, B):
-    max_num = 0
-    
-    for i in pos:
-        checkA = True
-        checkB = True
-        
-        for j in range(len(A)):
-            if A[j] % i != 0:
-                checkA = False
-                break
-            
-            if B[j] % i == 0:
-                checkB = False
-                break
-        
-        if not checkA or not checkB:
-            continue
-            
-        max_num = max(max_num, i)
-    
-    return max_num
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+from math import gcd
+
+def get_gcd(arr):
+    g = arr[0]
+    for i in range(len(arr)):
+        g = gcd(g, arr[i])
+    return g
 
 
-def solution(arrayA, arrayB):    
-    arrayA.sort()
-    arrayB.sort()
-    
-    minA = arrayA[0]
-    minB = arrayB[0]
+def solution(arrayA, arrayB):
+    answer = 0
+    gA, gB = get_gcd(arrayA), get_gcd(arrayB)
 
-    maxx = 0
-    
-    posA = yaksoo(minA)
-    posB = yaksoo(minB)
-    
-    maxx = max(cal_max(posA, arrayA, arrayB), cal_max(posB, arrayB, arrayA))
-    
-    return maxx
+    for b in arrayB:
+        if not b % gA:
+            break
+    else:
+        answer = max(answer, gA)
+
+    for a in arrayA:
+        if not a % gB:
+            break
+    else:
+        answer = max(answer, gB)
+
+    return answer

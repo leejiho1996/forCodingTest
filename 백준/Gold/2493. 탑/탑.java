435fafce2c3b301a710ea06684b8a3641b0e7fa1
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-11-12 18:41:22 +0900

    [Gold V] Title: 탑, Time: 2504 ms, Memory: 131356 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Stack<Integer> stack = new Stack<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] towers = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            towers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int num = towers[i];
+
+            while (!stack.isEmpty()) {
+                if (towers[stack.peek()] > num) {
+                    result[i] = stack.peek() + 1;
+                    break;
+                } else {
+                    stack.pop();
+                }
+            }
+            stack.push(i);
+        }
+        for (int e: result) {
+            System.out.print(e + " ");
+        }
+    }
+}

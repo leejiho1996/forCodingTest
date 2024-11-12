8106c5da30f7fd301650de3086beee2a2997b912
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-11-12 20:40:23 +0900

    [level 2] Title: 우박수열 정적분, Time: 1.94 ms, Memory: 12.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.py
@@ -0,0 +1,30 @@
+def solution(k, ranges):
+    answer = []
+    points = [k]
+    areas = [0]
+    
+    while k != 1:
+        if k % 2 == 0:
+            k //= 2
+        elif k % 2 == 1:
+            k *= 3
+            k += 1
+            
+        area = (k + points[-1]) / 2
+        areas.append(areas[-1] + area)
+            
+        points.append(k)
+    
+    n = len(points) - 1
+    
+    for x, y in ranges:
+    
+        if y <= 0:
+            y = n + y
+        
+        if x > y or x > n:
+            answer.append(-1)
+        else:
+            answer.append(areas[y] - areas[x])
+
+    return answer

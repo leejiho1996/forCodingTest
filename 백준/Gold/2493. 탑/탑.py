53c7e4f2442b31420216b6ec327f5b78c80efd8e
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-11-12 18:04:20 +0900

    [Gold V] Title: 탑, Time: 620 ms, Memory: 114296 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -5,22 +5,17 @@ input = sys.stdin.readline
 n = int(input())
 towers = list(map(int,input().split()))
 MAX = 100000001
-result = [0] * (n+1)
-result[0] = MAX
+towers += [MAX]
+result = [0] * (n+2)
 
 stack = [(0, MAX), (1, towers[0])]
 
-for i in range(1, n):
+for i in range(1, n+1):
     if towers[i] > stack[-1][1]:
         while stack and stack[-1][1] < towers[i]:
             idx, num = stack.pop()
             result[idx] = stack[-1][0]
 
     stack.append((i+1, towers[i]))
-    
-            
-while len(stack) > 1:
-    idx, num = stack.pop()
-    result[idx] = stack[-1][0]
 
-print(*result[1:])
+print(*result[1:n+1])

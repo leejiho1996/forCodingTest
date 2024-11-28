76b49f0f45cb21367132f7a7570768cc9957ab8a
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-11-29 00:23:37 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 118.83 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -0,0 +1,52 @@
+def toBinary(n):
+    result = []
+    while n > 1:
+        result.append(n % 2)
+        n //= 2
+    
+    result.append(n)
+    
+    return result[::-1]
+
+def divide(n):
+    stack = [n]
+    
+    while stack:
+        cur = stack.pop()
+            
+        half = len(cur) // 2
+        
+        if cur[half] == 0:
+            return 0
+        
+        right = cur[:half]
+        left = cur[half+1:]
+        
+        if len(cur) > 3:
+            if len(set(right)) > 1:
+                stack.append(right)
+            
+            if len(set(left)) > 1:
+                stack.append(left)
+                
+    return 1
+
+def solution(numbers):
+    answer = []
+    
+    for i in numbers:
+        binary = toBinary(i)
+        length = len(binary)
+        n = 1
+        
+        while True:
+            if 2**(n) - 1 < length:
+                n += 1
+            else:
+                break
+        
+        binary = [0] * ((2**n - 1) - length) + binary
+        
+        answer.append(divide(binary))
+    
+    return answer

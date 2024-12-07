4f46c025da60c29c8a85b6365c0c362d78629a2c
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-12-08 02:31:24 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 60 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
@@ -0,0 +1,38 @@
+# 가장 가까운 공통 조상
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    parent = [-1] * (n+1)
+    visited = [0] * (n+1)
+    
+    for j in range(n-1):
+        p, c = map(int,input().split())
+        parent[c] = p
+
+    a, b = map(int,input().split())
+
+    stack_a = [a]
+    stack_b = [b]
+
+    parent_a = set()
+    parent_b = set()
+    
+    while stack_a:
+        p_a = stack_a.pop()
+        visited[p_a] = 1
+
+        if parent[p_a] != -1:
+            stack_a.append(parent[p_a])
+
+    while stack_b:
+        p_b = stack_b.pop()
+
+        if visited[p_b]:
+            print(p_b)
+            break
+
+        stack_b.append(parent[p_b])

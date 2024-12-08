352d4b6fe64a0f035ad0c56b755da13af9f7fd8a
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-12-09 02:57:44 +0900

    [Gold I] Title: 합성함수와 쿼리, Time: 2352 ms, Memory: 75364 KB -BaekjoonHub

---
+++ 백준/Gold/17435. 합성함수와 쿼리/합성함수와 쿼리.py
@@ -0,0 +1,23 @@
+# 합성함수 쿼리
+import sys
+input = sys.stdin.readline
+
+m = int(input())
+func = list(map(int,input().split()))
+q = int(input())
+maxx = 20
+matrix = [[-1] * (m+1) for _ in range(maxx)]
+matrix[0] = [0] + func
+
+for i in range(1, maxx):
+    for j in range(1, m+1):
+        matrix[i][j] = matrix[i-1][matrix[i-1][j]]
+
+for i in range(q):
+    n, x = map(int,input().split())
+    cur = x
+
+    for i in range(maxx, -1, -1):
+        if n & 1 << i:
+            cur = matrix[i][cur]
+    print(cur)

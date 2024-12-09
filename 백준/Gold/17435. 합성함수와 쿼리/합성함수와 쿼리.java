25e19e599b863e5e99ed3a0d53d2a0416b4ba6e2
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-12-09 22:44:38 +0900

    [Gold I] Title: 합성함수와 쿼리, Time: 1940 ms, Memory: 118588 KB -BaekjoonHub

---
+++ 백준/Gold/17435. 합성함수와 쿼리/합성함수와 쿼리.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int max = 20;
+        st = new StringTokenizer(br.readLine());
+        int[] func = new int[m+1];
+
+        for (int i = 1; i <= m; i++){
+            func[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int q = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
+        int[][] matrix = new int[max][m+1];
+
+        for (int i = 1; i <= m; i++) {
+            matrix[0][i] = func[i];
+        }
+
+        for (int i = 1; i < max; i++) {
+            for (int j = 1; j <= m; j++) {
+                matrix[i][j] = matrix[i-1][matrix[i-1][j]];
+            }
+        }
+
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int cur = x;
+
+            for (int j = max; j >= 0; j--) {
+                if ((n & (1 << j)) != 0) {
+                    cur = matrix[j][cur];
+                }
+            }
+            System.out.println(cur);
+        }
+
+    }
+}

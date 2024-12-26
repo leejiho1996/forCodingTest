776594a42fa35844dc9d626892eb79caeeef5b68
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-12-26 21:11:47 +0900

    [Gold III] Title: 줄 세우기, Time: 656 ms, Memory: 49280 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        ArrayList<Integer>[] child = new ArrayList[n+1];
+        int[] count = new int[n+1];
+        int[] result = new int[n];
+        int cnt = 0;
+
+        for (int i = 0; i <= n; i++) {
+            child[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            child[A].add(B);
+            count[B] += 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (count[i] == 0) {
+                stack.push(i);
+            }
+        }
+
+        while (stack.size() > 0) {
+            int cur = stack.pop();
+            result[cnt++] = cur;
+
+            for (int i : child[cur]) {
+                if (--count[i] == 0) {
+                    stack.push(i);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}

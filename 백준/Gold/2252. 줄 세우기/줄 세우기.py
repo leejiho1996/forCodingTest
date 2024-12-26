e02accc32c16d7e4c08b65c5aac455d56ba2f549
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2024-12-26 20:42:24 +0900

    [Gold III] Title: 줄 세우기, Time: 188 ms, Memory: 39580 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,37 +1,30 @@
 # 줄 세우기
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(100001)
 
 n, m = map(int,input().split())
-
-graph = [[] for _ in range(n+1)]
-depth = [[-1, i] for i in range(n+1)]
+child = [[] for _ in range(n+1)]
+count = [0] * (n+1)
+stack = []
+result = []
 
 for i in range(m):
-    front, end = map(int,input().split())
-    graph[end].append(front)
-
-def dfs(num):
-    if depth[num][0] != -1:
-        return depth[num][0]
-
-    if len(graph[num]) == 0:
-        depth[num][0] = 1
-        return 1
-
-    for i in (graph[num]):
-        if depth[i][0] != -1:
-            depth[num][0] = max(depth[num][0], depth[i][0] + 1)
-        else:
-            depth[num][0] = max(depth[num][0], dfs(i) + 1)
-    
-    return depth[num][0]
+    A, B = map(int,input().split())
+    count[B] += 1
+    child[A].append(B)
 
 for i in range(1, n+1):
-    num = dfs(i)
+    if not count[i]:
+        stack.append(i)
 
-depth.sort()
+while stack:
+    cur = stack.pop()
+    result.append(cur)
 
-for i in range(1, n+1):
-    print(depth[i][1], end=' ')
+    for i in child[cur]:
+        count[i] -= 1
+
+        if not count[i]:
+            stack.append(i)
+
+print(*result)

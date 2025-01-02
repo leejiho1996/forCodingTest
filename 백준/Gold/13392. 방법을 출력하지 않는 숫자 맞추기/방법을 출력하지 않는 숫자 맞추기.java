466eb300b64fad9293607f0c2b711ea8577392fa
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-01-02 22:24:17 +0900

    [Gold I] Title: 방법을 출력하지 않는 숫자 맞추기, Time: 112 ms, Memory: 14804 KB -BaekjoonHub

--- 백준/Gold/13392. 방법을 출력하지 않는 숫자 맞추기/방법을 출력하지 않는 숫자 맞추기.java
+++ 백준/Gold/13392. 방법을 출력하지 않는 숫자 맞추기/방법을 출력하지 않는 숫자 맞추기.java
@@ -1,44 +1,31 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int result = 100001;
         int[][] dp = new int[n+1][10];
-        int[] start = new int[n+1];
-        int[] target = new int[n+1];
+        int[] start = new int[n];
+        int[] target = new int[n];
         String strStart = br.readLine();
         String strTarget = br.readLine();
 
-        for (int i = 1; i <= n; i++) {
-            start[i] = strStart.charAt(i - 1) - '0';
-            target[i] = strTarget.charAt(i - 1) - '0';
+        for (int i = 0; i < n; i++) {
+            start[i] = strStart.charAt(i) - '0';
+            target[i] = strTarget.charAt(i) - '0';
         }
 
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(dp[i], 100001);
-        }
-
-        for (int i = 0; i < 10; i++) {
-            dp[0][i] = i;
-        }
-
-        for (int i = 1; i <= n; i++) {
+        for (int i = n-1; i >= 0; i--) {
             for (int j = 0; j < 10; j++) {
                 int left = (target[i] - start[i] - j + 20) % 10;
                 int right = 10 - left;
 
-                dp[i][j] = Math.min(dp[i][j], dp[i-1][j] + right);
-                dp[i][(j+left) % 10] = Math.min(dp[i][(j+left) % 10], dp[i-1][j] + left);
+                dp[i][j] = Math.min(dp[i+1][j] + right, dp[i+1][(j+left) % 10] + left);
             }
         }
-
-        for (int i = 0; i < 10; i++) {
-            result = Math.min(result, dp[n][i]);
-        }
-
-        System.out.println(result);
+        System.out.println(dp[0][0]);
     }
 }

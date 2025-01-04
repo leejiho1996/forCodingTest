1a0b9fc0e85048734c0dd3c1aba53ff34cbaf30b
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-01-04 21:43:27 +0900

    [Platinum V] Title: 박성원, Time: 364 ms, Memory: 139544 KB -BaekjoonHub

--- 백준/Platinum/1086. 박성원/박성원.py
+++ 백준/Platinum/1086. 박성원/박성원.py
@@ -1,76 +1,33 @@
-# 박성원
-import sys
-input = sys.stdin.readline
+from math import factorial
+n=int(input())
+l=[int(input()) for _ in range(n)]
+k=int(input())
+list1=[(i%k,len(str(i))) for i in l]
 
-num_list = []
-n = int(input())
-
-for i in range(n):
-    num_list.append(input().rstrip())
-
-div = int(input())
-num_len = []
-div_list = []
-ten_div = [0]
-
-for i in num_list:
-    num_len.append(len(i))
-    div_list.append(int(i) % div)
-
-dp = [[-1] * div for _ in range(2**n)]
-
-ten = 10
-for i in range(max(num_len)):
-    ten_div.append(ten % div)
-    ten *= 10
-    
-def gcd(a, b):
-    if a < b:
-        a, b = b, a
-        
-    while b != 0:
-        a, b = b, a % b
-
-    return a
-
-def cal_remain(remain, idx):
-    if remain == 0:
-        return div_list[idx]
-    
-    return ((remain * (ten_div[num_len[idx]]) + (div_list[idx]))) % div
-
-def dfs(visited, remain):
-    
-    if visited == (1 << n) - 1:
-    
-        if remain == 0:
-            return 1
-        else:
-            return 0
-    
-    if dp[visited][remain] != -1:
-        return dp[visited][remain]
-    
-    for i in range(n):
-        if visited & 1 << i:
+dp=[[0]*k for _ in range(1<<n)]
+dp[0][0]=1
+length=[0]*(1<<n)
+length[0]=1
+for i in range(1<<n):
+    for j in range(n):
+        if i&(1<<j):
             continue
-        
-        if dp[visited][remain] == -1:
-            dp[visited][remain] = 0
-            
-        dp[visited][remain] += dfs(visited | 1 << i, cal_remain(remain, i))
+        bit=i|(1<<j)
+        #시간 줄이기 1: length 리스트를 dp 리스트와 함께 갱신
+        #시간 줄이기 2: length 리스트를 자릿수의 합이 아닌, 10^(자릿수의 합)%k로 설정
+        if length[bit] != 0:
+            pass
+        else:
+            length[bit] = length[i] * pow(10, list1[j][1], k) % k
+        for m in range(k): #시간 줄이기 3: dp[bit]의 모든 원소를 한번에 갱신
+            dp[bit][m] += dp[i][(m-list1[j][0] * length[i]) % k]
 
-    return dp[visited][remain]
+def gcd(a,b):
+    r=a%b
+    while r:
+        a,b=b,r
+        r=a%b
+    return b
 
-numerator = dfs(0, 0)
-
-denominator = 1
-for i in range(1, n+1):
-    denominator *= i
-
-gcd = gcd(numerator, denominator)
-
-numerator //= gcd
-denominator //= gcd
-
-print(f'{numerator}/{denominator}')
+g=gcd(dp[-1][0],factorial(n))
+print(f'{dp[-1][0]//g}/{factorial(n)//g}')

868da7e5f6a133593faaa7786801353f7171d14f
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-01-08 20:41:59 +0900

    [Gold V] Title: 숫자고르기, Time: 112 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -1,38 +1,12 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
     static int n;
     static int[] down;
     static boolean[] visited;
     static List<Integer> result;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // 입력 받기
-        n = sc.nextInt();
-        down = new int[n + 1];
-        visited = new boolean[n + 1];
-        result = new ArrayList<>();
-
-        for (int i = 1; i <= n; i++) {
-            down[i] = sc.nextInt();
-        }
-
-        // 각 숫자에서 DFS 실행
-        for (int i = 1; i <= n; i++) {
-            if (dfs(i, i)) {
-                result.add(i);
-            }
-        }
-
-        // 결과 출력
-        System.out.println(result.size());
-        for (int num : result) {
-            System.out.println(num);
-        }
-    }
-
+    
     // DFS 구현
     private static boolean dfs(int start, int current) {
         if (visited[current]) {
@@ -50,5 +24,32 @@ public class Main {
         boolean isCycle = dfs(start, child);
         visited[current] = false;
         return isCycle;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // 입력 받기
+        n = Integer.parseInt(br.readLine());
+        down = new int[n + 1];
+        visited = new boolean[n + 1];
+        result = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            down[i] = Integer.parseInt(br.readLine());
+        }
+
+        // 각 숫자에서 DFS 실행
+        for (int i = 1; i <= n; i++) {
+            if (dfs(i, i)) {
+                result.add(i);
+            }
+        }
+
+        // 결과 출력
+        System.out.println(result.size());
+        for (int num : result) {
+            System.out.println(num);
+        }
     }
 }

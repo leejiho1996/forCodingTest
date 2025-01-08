16da0799f1db7ae0ed6b7c9f62014ffb64d6802c
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-01-08 20:45:12 +0900

    [Gold V] Title: 숫자고르기, Time: 104 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -7,16 +7,15 @@ public class Main {
     static boolean[] visited;
     static List<Integer> result;
     
-    // DFS 구현
     private static boolean dfs(int start, int current) {
         if (visited[current]) {
             return false;
         }
 
         visited[current] = true;
-        int child = down[current];
+        int child = down[current]; 
 
-        if (start == child) {
+        if (start == child) { // 다음 노드와 출발 노드와 같다면 종료
             visited[current] = false;
             return true;
         }
@@ -28,12 +27,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         
         // 입력 받기
         n = Integer.parseInt(br.readLine());
         down = new int[n + 1];
         visited = new boolean[n + 1];
-        result = new ArrayList<>();
+        int cnt = 0;
         
         for (int i = 1; i <= n; i++) {
             down[i] = Integer.parseInt(br.readLine());
@@ -42,14 +42,13 @@ public class Main {
         // 각 숫자에서 DFS 실행
         for (int i = 1; i <= n; i++) {
             if (dfs(i, i)) {
-                result.add(i);
+                sb.append(i).append("\n");
+                cnt += 1;
             }
         }
 
         // 결과 출력
-        System.out.println(result.size());
-        for (int num : result) {
-            System.out.println(num);
-        }
+        System.out.println(cnt);
+        System.out.println(sb);
     }
 }

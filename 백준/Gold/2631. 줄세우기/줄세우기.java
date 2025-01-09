75e5f0ab9b9aa6d224f3e98ffaa5227be326a0f1
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-01-09 22:31:45 +0900

    [Gold IV] Title: 줄세우기, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.java
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -1,38 +1,30 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 입력 개수
-        List<Integer> lis = new ArrayList<>(); // LIS를 저장할 리스트
+        int n = Integer.parseInt(br.readLine()); 
+        int[] dp = new int[n];
+        int[] nums = new int[n];
 
         for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(br.readLine()); // 입력 값
-            int start = 0;
-            int end = lis.size() - 1;
-
-            // 리스트가 비어 있는 경우, 값을 추가
-            if (lis.isEmpty() || lis.get(lis.size() - 1) < num) {
-                lis.add(num);
-                continue;
-            }
-
-            // 이진 탐색으로 적절한 위치를 찾기
-            while (start <= end) {
-                int mid = (start + end) / 2;
-                if (lis.get(mid) >= num) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
+            nums[i] = Integer.parseInt(br.readLine());
+            dp[i] = 1; 
+        }
+      
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-
-            // 찾은 위치에 현재 값을 삽입
-            lis.set(start, num);
+        }
+       
+        int lisLength = 0;
+        for (int length : dp) {
+            lisLength = Math.max(lisLength, length);
         }
 
-        // 결과 출력
-        System.out.println(n - lis.size());
+        System.out.println(n - lisLength);
     }
 }

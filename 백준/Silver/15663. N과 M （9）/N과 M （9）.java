d69f65bd5216c36d186488eb62da6200bab28a79
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-01-13 14:12:35 +0900

    [Silver II] Title: N과 M (9), Time: 344 ms, Memory: 21452 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,14 +1,10 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     static int n, m;
     static int[] nums;
     static boolean[] visited;
-    static Set<String> sequenceSet = new HashSet<>();
-    static List<Integer> currentSequence = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,32 +23,27 @@ public class Main {
 
         Arrays.sort(nums);
 
-        backtrack(0);
+        backtrack("", 0);
     }
 
-    static void backtrack(int count) {
+    static void backtrack(String st, int count) {
         if (count == m) {
-            StringBuilder sb = new StringBuilder();
-            for (int num : currentSequence) {
-                sb.append(num).append(" ");
-            }
-
-            String result = sb.toString().trim();
-            if (!sequenceSet.contains(result)) {
-                sequenceSet.add(result);
-                System.out.println(result);
-            }
+            System.out.println(st);
             return;
         }
 
+        int prev = -1;
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                currentSequence.add(nums[i]);
-                backtrack(count + 1);
-                currentSequence.remove(currentSequence.size() - 1);
-                visited[i] = false;
+            if (visited[i] || nums[i] == prev) {
+                continue;
             }
+
+            prev = nums[i];
+            visited[i] = true;
+            String next = st.length() == 0 ? ""+nums[i] : st + " " + nums[i];
+            backtrack(next, count + 1);
+
+            visited[i] = false;
         }
     }
 }

520c846a8cd1b88cff1ae48f9318e3d215371c5e
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-01-23 20:50:48 +0900

    [Gold III] Title: 웜홀, Time: 2444 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -2,35 +2,34 @@
 import sys
 input = sys.stdin.readline
 
-def floyd():
-    for k in range(1, n+1):
-        for start in range(1, n+1):
-            for to in range(1, n+1):
-                if graph[start][k] + graph[k][to] < graph[start][to]:
-                    graph[start][to] = graph[start][k] + graph[k][to]
-
-                if start == to and graph[start][to] < 0:
-                    return True
-    return False
-
 t = int(input())
 
 for i in range(t):
     n, m, w = map(int,input().split())
-    graph = [[10001] * (n+1) for _ in range(n+1)]
-    wormhole = []
+    upper = [0] * (n+1)
+    edges = []
     check = False
     
     for j in range(m):
         s, e, t = map(int,input().split())
-        graph[s][e] = graph[e][s] = min(graph[s][e], t)
+        edges.append((s,e,t))
+        edges.append((e,s,t))
         
     for j in range(w):
         s, e, t = map(int,input().split())
-        graph[s][e] = min(graph[s][e], -t)
+        edges.append((s,e,-t))
+        
+    for j in range(n):
+        for start, to, time in edges:
+            if upper[start] + time < upper[to]:
+                upper[to] = upper[start] + time
 
-   
-    if floyd():
+    for start, to, time in edges:
+        if upper[start] + time < upper[to]:
+                check = True
+                break
+    
+    if check:
         print("YES")
     else:
         print("NO")

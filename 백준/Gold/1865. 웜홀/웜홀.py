f390d200a0a694256cf87594d78a1fefdbe11652
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-01-23 21:08:44 +0900

    [Gold III] Title: 웜홀, Time: 912 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -2,13 +2,24 @@
 import sys
 input = sys.stdin.readline
 
+def bellmanford(n, upper, edges):
+    for j in range(n-1):
+        for start, to, time in edges:
+            if upper[start] + time < upper[to]:
+                upper[to] = upper[start] + time
+
+    for start, to, time in edges:
+        if upper[start] + time < upper[to]:
+                return True
+
+    return False
+    
 t = int(input())
 
 for i in range(t):
     n, m, w = map(int,input().split())
     upper = [0] * (n+1)
     edges = []
-    check = False
     
     for j in range(m):
         s, e, t = map(int,input().split())
@@ -19,16 +30,7 @@ for i in range(t):
         s, e, t = map(int,input().split())
         edges.append((s,e,-t))
         
-    for j in range(n-1):
-        for start, to, time in edges:
-            if upper[start] + time < upper[to]:
-                upper[to] = upper[start] + time
-
-    for start, to, time in edges:
-        if upper[start] + time < upper[to]:
-                check = True
-                break
-    if check:
+    if bellmanford(n, upper, edges):
         print("YES")
     else:
         print("NO")

08696cd88bf43c0974d8384c42d603bef92df7a6
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-01-25 14:50:08 +0900

    [Platinum V] Title: 숫자 맞추기, Time: 180 ms, Memory: 39576 KB -BaekjoonHub

---
+++ 백준/Platinum/2494. 숫자 맞추기/숫자 맞추기.py
@@ -0,0 +1,55 @@
+# 숫자 맞추기
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+start = [0] + [int(i) for i in input().rstrip()]
+target = [0] + [int(i) for i in input().rstrip()]
+
+dp = [[1000000] * 10 for _ in range(n+1)]
+seq = [[0] * 10 for _ in range(n+1)]
+
+for i in range(10):
+    dp[0][i] = i
+
+for i in range(1, n+1):
+    for j in range(10):
+        left = (target[i] - start[i] - j + 20) % 10
+        right = 10 - left
+        nextLeft = (j + left) % 10
+        
+        # 오른쪽 회전
+        if dp[i][j] > dp[i-1][j] + right:
+            dp[i][j] = dp[i-1][j] + right
+            seq[i][j] = -right
+
+        # 왼쪽 회전
+        if dp[i][nextLeft] > dp[i-1][j] + left:
+            dp[i][nextLeft] = dp[i-1][j] + left
+            seq[i][nextLeft] = left
+
+result = 1000000
+resultSeq = []
+cnt = n
+
+for i in range(10):
+    if result > dp[n][i]:
+        result = dp[n][i]
+        idx = i
+
+print(result)
+
+while cnt > 0:
+    move = seq[cnt][idx]
+    resultSeq.append(move)
+
+    if move > 0:
+        idx = (idx - move + 10) % 10
+
+    cnt -= 1
+
+for i in range(n):
+    print(i+1, resultSeq[n-i-1])
+    
+    
+    

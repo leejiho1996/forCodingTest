e9725a05e4be8bc4c598846191f5ff0a737df7b8
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-01-25 15:03:05 +0900

    [Platinum V] Title: 숫자 맞추기, Time: 164 ms, Memory: 39576 KB -BaekjoonHub

--- 백준/Platinum/2494. 숫자 맞추기/숫자 맞추기.py
+++ 백준/Platinum/2494. 숫자 맞추기/숫자 맞추기.py
@@ -6,7 +6,7 @@ n = int(input())
 start = [0] + [int(i) for i in input().rstrip()]
 target = [0] + [int(i) for i in input().rstrip()]
 
-dp = [[1000000] * 10 for _ in range(n+1)]
+dp = [[100000] * 10 for _ in range(n+1)]
 seq = [[0] * 10 for _ in range(n+1)]
 
 for i in range(10):
@@ -21,14 +21,14 @@ for i in range(1, n+1):
         # 오른쪽 회전
         if dp[i][j] > dp[i-1][j] + right:
             dp[i][j] = dp[i-1][j] + right
-            seq[i][j] = -right
+            seq[i][j] = -right # 회전횟수 갱신
 
         # 왼쪽 회전
         if dp[i][nextLeft] > dp[i-1][j] + left:
             dp[i][nextLeft] = dp[i-1][j] + left
-            seq[i][nextLeft] = left
+            seq[i][nextLeft] = left # 회전횟수 갱신
 
-result = 1000000
+result = 100000
 resultSeq = []
 cnt = n
 
@@ -37,7 +37,7 @@ for i in range(10):
         result = dp[n][i]
         idx = i
 
-print(result)
+print(result) # 총 회전횟수 출력
 
 while cnt > 0:
     move = seq[cnt][idx]
@@ -49,7 +49,4 @@ while cnt > 0:
     cnt -= 1
 
 for i in range(n):
-    print(i+1, resultSeq[n-i-1])
-    
-    
-    
+    print(i+1, resultSeq[n-i-1]) # 숫자 나사번호별 회전횟수 출력

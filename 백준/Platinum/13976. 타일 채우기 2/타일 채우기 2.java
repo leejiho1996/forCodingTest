7701e1b9850bd81acb5cd45286f4d79f54b8b4d2
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-01-28 19:03:39 +0900

    [Platinum V] Title: 타일 채우기 2, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Platinum/13976. 타일 채우기 2/타일 채우기 2.java
+++ 백준/Platinum/13976. 타일 채우기 2/타일 채우기 2.java
@@ -26,7 +26,7 @@ public class Main {
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
                 for (int k = 0; k < 2; k++) {
-                    result[i][j] += (m1[i][k] * m2[k][j] + MOD) % MOD ;
+                    result[i][j] += (m1[i][k] * m2[k][j] + MOD);
                 }
                 result[i][j] %= MOD;
             }

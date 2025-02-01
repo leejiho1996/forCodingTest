859acdb0b7b20c0add954a2334bfa18d490e2ef1
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-02-01 17:51:56 +0900

    [Gold III] Title: 치즈, Time: 680 ms, Memory: 35000 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,23 +1,20 @@
 # 치즈
 import sys
 input = sys.stdin.readline
-from collections import deque
 sys.setrecursionlimit(10001)
 
 def dfs(r, c):
-    graph[r][c] = 3
-    visited[r][c] = 1
     
     for x, y in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
         nr, nc = r + x, c + y
 
         if nr < 0 or nr >= n or nc < 0 or nc >= m:
             continue
-        elif visited[nr][nc]:
-            continue
-        elif graph[nr][nc] == 0 or graph[nr][nc] == 3:
+        elif graph[nr][nc] == 1:
+            visited[nr][nc] += 1
+        elif not visited[nr][nc]:
+            visited[nr][nc] = 1
             dfs(nr, nc)
-
 
 n, m = map(int, input().split())
 graph = []
@@ -31,22 +28,12 @@ for i in range(n):
 while True:
     visited = [[0] * m for _ in range(n)]
     melt = []
+    visited[0][0] = 1
     dfs(0, 0)
 
     for r in range(n):
         for c in range(m):
-            if graph[r][c] != 1:
-                continue
-            cnt = 0
-            for x, y in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-                nr, nc = r + x, c + y
-                if nr < 0 or nr >= n or nc < 0 or nc >= m:
-                    continue
-
-                if graph[nr][nc] == 3:
-                    cnt += 1
-
-            if cnt >= 2:
+            if visited[r][c] >= 2:
                 melt.append((r, c))
 
     if not melt:
@@ -57,5 +44,4 @@ while True:
 
     time += 1
     
-
 print(time)

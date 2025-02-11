f82ce70820bbc7bf179a451e78d49b8997811aab
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-02-11 17:50:38 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2136 ms, Memory: 49360 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -6,14 +6,14 @@ t = int(input())
 
 for i in range(t):
     n = int(input())
-    graph = [[] for _ in range(n+1)]
+    graph = [0] * (n+1)
     students = list(map(int,input().split()))
     front = [0] * (n+1)
     stack = []
     cnt = 0
     
     for j in range(n):
-        graph[j+1].append(students[j])
+        graph[j+1] = students[j]
         front[students[j]] += 1
         
     for i in range(1, n+1):
@@ -24,10 +24,10 @@ for i in range(t):
         cur = stack.pop()
         cnt += 1
 
-        for k in graph[cur]:
-            front[k] -= 1
+        nex = graph[cur]
+        front[nex] -= 1
 
-            if front[k] == 0:
-                stack.append(k)
+        if front[nex] == 0:
+            stack.append(nex)
 
     print(cnt)

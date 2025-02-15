5eb86939dd1bc3397930701236cc71f6273bc48a
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-02-16 03:58:58 +0900

    [Gold II] Title: 선분 교차 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.py
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.py
@@ -1,37 +1,50 @@
+# 선분 교차2
 import sys
 input = sys.stdin.readline
 
 def ccw(a, b, c):
     ab = [b[0] - a[0], b[1] - a[1]]
     ac = [c[0] - a[0], c[1] - a[1]]
+
     product = ab[0] * ac[1] - ab[1] * ac[0]
 
-    if product > 0:
-        return 1  # 반시계 방향
-    elif product < 0:
-        return -1  # 시계 방향
-    else:
-        return 0  # 일직선
+    return product
 
-def is_intersect(a, b, c, d):
-    AB = ccw(a, b, c) * ccw(a, b, d)
-    CD = ccw(c, d, a) * ccw(c, d, b)
+def sortVector(x1, y1, x2, y2):
+    
+    if x2 < x1:
+        x1, x2 = x2, x1
+        y1, y2 = y2, y1
+    elif x1 == x2 and y2 < y1:
+        x1, x2 = x2, x1
+        y1, y2 = y2, y1
 
+    return [x1, y1, x2, y2]
+
+        
+x1, y1, x2, y2 = map(int,input().split())
+x3, y3, x4, y4 = map(int,input().split())
+
+sortAB = sortVector(x1, y1, x2, y2)
+sortCD = sortVector(x3, y3, x4, y4)
+
+A = sortAB[:2]
+B = sortAB[2:]
+C = sortCD[:2]
+D = sortCD[2:]
+
+AB = ccw(A, B, C) * ccw(A, B, D)
+CD = ccw(C, D, A) * ccw(C, D, B)
+
+if AB <= 0 and CD <= 0:
+        
     if AB == 0 and CD == 0:
-        # 두 선분이 일직선상에 있을 경우, 겹치는지 확인
-        return max(a, c) <= min(b, d)  # x, y 기준 비교
-    return AB <= 0 and CD <= 0
-
-x1, y1, x2, y2 = map(int, input().split())
-x3, y3, x4, y4 = map(int, input().split())
-
-# A, B 정렬
-A = (x1, y1)
-B = (x2, y2)
-C = (x3, y3)
-D = (x4, y4)
-
-if A > B: A, B = B, A
-if C > D: C, D = D, C
-
-print(1 if is_intersect(A, B, C, D) else 0)
+        if not(B < C or D < A):
+            print(1)
+        else:
+            print(0)
+    else:
+        print(1)
+else:
+    print(0)
+            

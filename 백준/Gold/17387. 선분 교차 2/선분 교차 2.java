df0fdd60703700bbd4c36ce647ecd149b96fec9a
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-02-17 01:26:03 +0900

    [Gold II] Title: 선분 교차 2, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] A = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        int[] B = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] C = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        int[] D = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+
+        if (Arrays.compare(B, A) < 0) {
+            int[] tmp = A;
+            A = B;
+            B = tmp;
+        }
+
+        if (Arrays.compare(D, C) < 0) {
+            int[] tmp = C;
+            C = D;
+            D = tmp;
+        }
+
+        int AB = ccw(A, B, C) * ccw(A, B, D);
+        int CD = ccw(C, D, A) * ccw(C, D, B);
+
+        if (AB <= 0 && CD <= 0) {
+            if (AB == 0 && CD == 0) {
+                if (!(Arrays.compare(B, C) == -1 || Arrays.compare(D, A) == -1)) {
+                    System.out.println(1);
+                } else{
+                    System.out.println(0);
+                }
+            }
+            else {
+                System.out.println(1);
+            }
+        } else {
+            System.out.println(0);
+        }
+
+    }
+
+    static int ccw(int[] a, int[] b, int[] c) {
+        int[] ab = new int[] {b[0] - a[0], b[1] - a[1]};
+        int[] ac = new int[] {c[0] - a[0], c[1] - a[1]};
+
+        long product = (long) ab[0] * ac[1] - (long) ab[1] * ac[0];
+
+        if (product < 0) {
+            return -1;
+        } else if (product > 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}

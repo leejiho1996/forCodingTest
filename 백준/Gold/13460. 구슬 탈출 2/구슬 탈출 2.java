035008363482cc1e74ffcb0034b8c05909f00bbf
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-02-16 22:25:25 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 216 ms, Memory: 40312 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -11,6 +11,27 @@ public class Main {
     static int[] dy = {0, 0, 1, -1};
     static int[] order = {0, 1, 0, 1};
 
+    static class Point { // 좌표를 나타낼 클래스
+        int r;
+        int c;
+
+        public Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+
+        public boolean equals(Point oppo) { // 두개의 좌표가 같은지 확인하는 메서드
+            if (this.r == oppo.r && this.c == oppo.c) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public boolean compare(Point oppo) { // 현재좌표가 비교하는 좌표보다 위, 왼쪽에 있는지 확인
+            return this.r != oppo.r ? this.r < oppo.r : this.c < oppo.c;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,11 +47,11 @@ public class Main {
             for (int j = 0; j < m; j++) {
                 graph[i][j] = row.charAt(j);
 
-                if (graph[i][j] == 'R') {
+                if (graph[i][j] == 'R') { // R 좌표 저장
                     red = new Point(i, j);
-                } else if (graph[i][j] == 'B') {
+                } else if (graph[i][j] == 'B') { // B 좌표 저장
                     blue = new Point(i, j);
-                } else if (graph[i][j] == 'O') {
+                } else if (graph[i][j] == 'O') { // O 좌표 저장
                     out = new Point(i, j);
                 }
             }
@@ -38,62 +59,38 @@ public class Main {
 
         backtrack(0, red, blue, -1);
 
-        if (result == 1000) {
+        if (result == 1000) { // result값이 바뀌지 않았다면 한번도 성공 못한 것 -1 출력
             System.out.println(-1);
         } else {
             System.out.println(result);
         }
     }
 
-    static class Point { // 좌표를 나타낼 클래스
-        int r;
-        int c;
-
-        public Point(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-
-        public boolean equals(Point oppo) {
-            if (this.r == oppo.r && this.c == oppo.c) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        public boolean compare(Point oppo) {
-            return this.r != oppo.r ? this.r < oppo.r : this.c < oppo.c;
-        }
-    }
-
     static void backtrack(int cnt, Point red, Point blue, int last) {
-        if (cnt > 10) {
+        if (cnt > 10) { // 움직인 횟수가 10번 초과라면 종료
             return;
         }
 
-        if (red.equals(out) && blue.equals(out)) {
+        if (blue.equals(out)) { // 파란구슬이 들어가면 실패
             return;
-        } else if (blue.equals(out)) {
-            return;
-        } else if (red.equals(out)) {
+        } else if (red.equals(out)) { // 빨간구슬이 들어갔다면 result 갱신후 return
             result = Math.min(result, cnt);
             return;
         }
 
         for (int i = 0; i < 4; i++) {
-            if (i == last) {
+            if (i == last) { // 직전에 탐색한 방향은 굳이 다시 할 필요 없다
                 continue;
             }
 
-            int nx = dx[i];
-            int ny = dy[i];
-            int ord = order[i];
+            int nx = dx[i]; // 행 증가
+            int ny = dy[i]; // 열 증가
+            int ord = order[i];  // ord가 0이면 아래나 오른쪽으로 이동, 1이면 위나 왼쪽으로 이동
 
             Point redAfter;
             Point blueAfter;
 
-            if (ord == 1) {
+            if (ord == 1) { // 위나 왼쪽으로 이동은 위나 왼쪽에 존재하는 구슬이 먼저 움직어야 한다
                 if (red.compare(blue)) {
                     redAfter = moveBall(red, blue, nx, ny);
                     blueAfter = moveBall(blue, redAfter, nx, ny);
@@ -101,7 +98,7 @@ public class Main {
                     blueAfter = moveBall(blue, red, nx, ny);
                     redAfter = moveBall(red, blueAfter, nx, ny);
                 }
-            } else {
+            } else { // 아래, 오른쪽으로 이동은 아래나 오른쪽에 존재하는 구슬이 먼저 움직어야 한다
                 if (red.compare(blue)) {
                     blueAfter = moveBall(blue, red, nx, ny);
                     redAfter = moveBall(red, blueAfter, nx, ny);
@@ -119,11 +116,12 @@ public class Main {
         int c = p1.c;
 
         while (true) {
-            if (r+dx == out.r && c+dy == out.c) {
-                return new Point(r+dx, c+dy);
+            // 구슬이 구멍에 간다면 return
+            if (r + dx == out.r && c + dy == out.c) {
+                return new Point(r + dx, c + dy);
             }
-
-            else if (graph[r+dx][c+dy] == '#' || (r+dx == p2.r && c+dy == p2.c)) {
+            //  벽이나 다른 구슬을 만나면 return
+            else if (graph[r + dx][c + dy] == '#' || (r + dx == p2.r && c + dy == p2.c)) {
                 return new Point(r, c);
             }
 

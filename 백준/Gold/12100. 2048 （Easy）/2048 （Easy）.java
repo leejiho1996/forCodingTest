52cd3f23f25c30748263a261b35062c900dcd49f
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-02-18 01:41:59 +0900

    [Gold I] Title: 2048 (Easy), Time: 184 ms, Memory: 22332 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int callCount = 0;
+    static int callCount = 0; // backtrack 함수의 호출 횟수 카운트
     static int result = 0;
     static int[][] visited;
     static int N;
@@ -41,7 +41,7 @@ public class Main {
     static void backtrack(int cnt, int[][] graph) {
         callCount ++;
 
-        if (cnt == 5) {
+        if (cnt == 5) { // 5번 이동시 최댓값 갱신하고 return
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     result = Math.max(result, graph[i][j]);
@@ -50,7 +50,7 @@ public class Main {
             return;
         }
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++) { // 4방향으로 민다
             int dr = dx[i]; int dc = dy[i];
             int sr = rs[i]; int sc = cs[i];
 
@@ -62,14 +62,17 @@ public class Main {
     static int[][] move(int callCount, int dx, int dy, int sr, int sc, int[][] graph) {
         int[][] nGraph = new int[N][N];
 
+        // 현재 단계의 게임판 복사
         for (int i = 0 ; i < N ; i++) {
-            for (int j = 0; j < N; j++) {
-                nGraph[i][j] = graph[i][j];
-            }
+            nGraph[i] = Arrays.copyOf(graph[i], N);
         }
 
         int er, ec, rSep, cSep;
 
+        // 행열 탐색 진행방향 결정
+        // 역방향 탐색은 -1, 정방향 탐색은 1
+        // sr -> 행 탐색 시작점 er -> 행 탐색 끝점
+        // sc -> 열 탐색 시작점 ec -> 열 탐색 끝점
         if (sr == N-1) {
             er = -1; rSep = -1;
         } else {
@@ -95,11 +98,12 @@ public class Main {
                 while(true) {
                     if (isOut(r+dx, c+dy)) {
                         break;
-                    } else if (nGraph[r+dx][c+dy] == 0) {
+                    } else if (nGraph[r+dx][c+dy] == 0) { // 다음 칸이 0이라면 이동
                         nGraph[r+dx][c+dy] = cur;
                         nGraph[r][c] = 0;
+                    // 다음칸이 현재 숫자와 같고, 현재 단계에서 방문하지 않았다면 합친다
                     } else if (nGraph[r+dx][c+dy] == cur && visited[r+dx][c+dy] != callCount) {
-                        visited[r+dx][c+dy] = callCount;
+                        visited[r+dx][c+dy] = callCount; // 방문 체크
                         nGraph[r+dx][c+dy] = cur*2;
                         nGraph[r][c] = 0;
                     } else {
@@ -115,6 +119,7 @@ public class Main {
         return nGraph;
     }
 
+    // 범위를 벗어나는지 확인하는 메서드
     static boolean isOut(int r, int c) {
         if (r < 0 || c < 0 || r >= N || c >= N) return true;
         else return false;

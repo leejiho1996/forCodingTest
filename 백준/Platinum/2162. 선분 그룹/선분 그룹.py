36580ab061d4bc15e6b64561c33dd9b6c689f731
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-02-20 15:26:13 +0900

    [Platinum V] Title: 선분 그룹, Time: 2484 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Platinum/2162. 선분 그룹/선분 그룹.py
+++ 백준/Platinum/2162. 선분 그룹/선분 그룹.py
@@ -1,75 +1,84 @@
-# 라이브러리 세팅
+# 선분 그룹
 import sys
+input = sys.stdin.readline
 
-# ccw 함수
 def ccw(a, b, c):
-    return (b[0] - a[0]) * (c[1] - a[1]) - (c[0] - a[0]) * (b[1] - a[1])
+    ab = [b[0] - a[0], b[1] - a[1]]
+    ac = [c[0] - a[0], c[1] - a[1]]
 
-# 선분 교차여부 함수
-def if_cross(first, second):
+    product = ab[0] * ac[1] - ab[1] * ac[0]
 
-    # 변수 세팅
-    x1, y1, x2, y2 = map(int, first)
-    one = [x1, y1]
-    two = [x2, y2]
-    x3, y3, x4, y4 = map(int, second)
-    three = [x3, y3]
-    four = [x4, y4]
-
-    # 예외처리(같은 직선상에 있는 경우)
-    if (ccw(one, two, three) == 0) & (ccw(one, two, four) == 0) & (ccw(three, four, one) == 0) & (ccw(three, four, two) == 0):
-        line_1 = [one, two] 
-        line_2 = [three, four]
-        line_1.sort(key=lambda x:(x[0]))
-        line_2.sort(key=lambda x:(x[0]))
-        if (line_1[0][0] > line_2[1][0]) | (line_1[1][0] < line_2[0][0]):
-            return False
-        line_1.sort(key=lambda x:(x[1]))
-        line_2.sort(key=lambda x:(x[1]))
-        if (line_1[0][1] > line_2[1][1]) | (line_1[1][1] < line_2[0][1]):
-            return False
-        else:
-            return True
-
-    # ccw를 이용하여 교차여부 탐색
-    if (ccw(one, two, three) * ccw(one, two, four) <= 0) & (ccw(three, four, one) * ccw(three, four, two) <= 0):
-        return True
+    if product < 0:
+        return -1
+    elif product > 0:
+        return 1
     else:
-        return False
+        return 0
+
+def find(n):
+    if parent[n] != n:
+        parent[n] = find(parent[n])
+
+    return parent[n]
+
+def isGroup(i, j):
+    p1 = find(i)
+    p2 = find(j)
+
+    if p1 != p2:
+        parent[p1] = p2
+        groupCnt[p2] += groupCnt[p1]
+        groupCnt[p1] = 0
+
+def isCross(A, B, C, D):
+    AB = ccw(A, B, C) * ccw(A, B, D)
+    CD = ccw(C, D, A) * ccw(C, D, B)
     
-# 변수 세팅
-n = int(sys.stdin.readline())
-union = [i for i in range(n)]
+    if AB == 0 and CD == 0: # 같은 직선상에 있는 경우
+        if not(B < C or D < A):
+            return True
+    elif AB <= 0 and CD <= 0:            
+        return True
 
-# 루트 탐색 함수
-def findp(k):
-    if k == union[k]:
-        return k
-    union[k] = findp(union[k])
-    return union[k]
+    return False
+        
+n = int(input())
+parent = [i for i in range(n)]
+groupCnt = [1 for _ in range(n)]
+lines = []
+groups = 0
+maxGroup = 0
 
-# 유니온 파인드를통해 교차하는 선분
-line_list = []
 for i in range(n):
-    line = list(map(int, sys.stdin.readline().split()))
-    line_list.append(list(line))
+    x1, y1, x2, y2 = map(int,input().split())
+
+    if (x2, y2) < (x1, y1):
+        lines.append([x2, y2, x1, y1])
+    else:
+        lines.append([x1, y1, x2, y2]) 
+
+lines.sort()
+
+for i in range(n):
+    x1, y1, x2, y2 = lines[i]
+    A = [x1, y1]
+    B = [x2, y2]
+
     for j in range(i):
-        if if_cross(line, line_list[j]):
-            p1 = findp(i) 
-            p2 = findp(j)
-            if p1 != p2:
-                union[p1] = p2
+        x3, y3, x4, y4 = lines[j]
+        C = [x3, y3]
+        D = [x4, y4]
 
-# 루트 갱신
+        if D < A:
+            continue
+
+        if isCross(A, B, C, D):
+            isGroup(i, j)
+
 for i in range(n):
-    findp(i)
+    if groupCnt[i] != 0:
+        groups += 1
+        maxGroup = max(maxGroup, groupCnt[i])
 
-# 답세팅, 출력
-group = set(union)
-group_num = len(group)
-ma = 0
-for i in group:
-    k = union.count(i)
-    ma = max(ma, k)
-print(group_num)
-print(ma)
+print(groups)
+print(maxGroup)

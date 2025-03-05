1d78dd46deebf744215d158200053392dc811379
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-03-05 20:37:15 +0900

    [Gold V] Title: 보물섬, Time: 4980 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -3,41 +3,44 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-N, M = map(int,input().split())
-graph = []
-direc = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-maxx = 0
+def solution():
+    N, M = map(int,input().split())
+    graph = []
 
-for i in range(N):
-    row = list(input().rstrip())
-    graph.append(row)
+    for i in range(N):
+        row = list(input().rstrip())
+        graph.append(row)
 
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] != "L":
-            continue
-        
-        visited = [[0]*M for _ in range(N)]
-        que = deque([(i, j, 0)])
+    maxx = 0
+    direc = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-        while que:
-            r, c, cnt= que.popleft()
-
-            if visited[r][c]:
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] != "L":
                 continue
-            else:
-                visited[r][c] = 1
-                maxx = max(maxx, cnt)
+            
+            visited = [[False]*M for _ in range(N)]
+            que = deque([(i, j, 0)])
 
-            for dx, dy in direc:
-                nr, nc = r + dx, c + dy
+            while que:
+                r, c, cnt= que.popleft()
 
-                if nr < 0 or nc < 0 or nr >= N or nc >= M:
-                    continue
-                elif graph[nr][nc] == "W":
+                if visited[r][c]:
                     continue
                 else:
-                    que.append((nr, nc, cnt+1))
+                    visited[r][c] = True
+                    maxx = max(maxx, cnt)
 
-print(maxx)
+                for dx, dy in direc:
+                    nr, nc = r + dx, c + dy
+
+                    if nr < 0 or nc < 0 or nr >= N or nc >= M:
+                        continue
+                    elif graph[nr][nc] == "W" or visited[nr][nc]:
+                        continue
+                    else:
+                        que.append((nr, nc, cnt+1))
+    return maxx
+
+print(solution())
         

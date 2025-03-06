af6da3805e21abc4530109a5a838eb2b31bd8d12
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-03-07 01:52:02 +0900

    [Platinum V] Title: 비숍, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Platinum/1799. 비숍/비숍.py
+++ 백준/Platinum/1799. 비숍/비숍.py
@@ -17,32 +17,27 @@ def backtrack(cnt, total, color):
 
     check = False
     for i in range(iterCnt):
-        rightDist = abs(0 - (sr+i)) + abs(N-1 - (sc-i))
+        dist = abs(0 - (sr+i)) + abs(N-1 - (sc-i))
 
-        if graph[sr+i][sc-i] == 0 or right[rightDist] == 1:
+        if graph[sr+i][sc-i] == 0 or visited[dist] == 1:
             continue
         
         check = True
-        right[rightDist] = 1
+        visited[dist] = 1
         backtrack(cnt+2, total+1, color)
-        right[rightDist] = 0
+        visited[dist] = 0
 
     if not check:
         backtrack(cnt+2, total, color)
         
 N = int(input())
 graph = []
-left = [0] * (N*2 - 1)
-right = [0] * (N*2 - 1)
+visited = [0] * (N*2 - 1)
 result = [0, 0]
 
 for i in range(N):
     row = list(map(int,input().split()))
     graph.append(row)
-
-    for j in range(N):
-        if row[j] == 1:
-            left[abs(0 - i) + abs(0 - j)] = 1
     
 backtrack(0, 0, 0)
 backtrack(1, 0, 1)

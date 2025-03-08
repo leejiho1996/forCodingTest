47e904cc7537987ef9bcfe02e5e7610c8d18440b
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-03-08 18:33:55 +0900

    [Platinum V] Title: 행성 터널, Time: 1316 ms, Memory: 102748 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.py
+++ 백준/Platinum/2887. 행성 터널/행성 터널.py
@@ -1,7 +1,6 @@
 # 행성 터널
 import sys
 input = sys.stdin.readline
-import heapq as hq
 
 def find(n):
     if parent[n] != n:
@@ -23,20 +22,20 @@ for i in range(N):
 
 X.sort(); Y.sort(); Z.sort(); # X, Y, Z 좌표 정렬
 
-que = []
+edges = []
 
 for i in range(N-1):
     distX = X[i+1][0] - X[i][0]
     distY = Y[i+1][0] - Y[i][0]
     distZ = Z[i+1][0] - Z[i][0]
 
-    hq.heappush(que, (distX, X[i+1][1], X[i][1]))
-    hq.heappush(que, (distY, Y[i+1][1], Y[i][1]))
-    hq.heappush(que, (distZ, Z[i+1][1], Z[i][1]))
+    edges.append((distX, X[i+1][1], X[i][1]))
+    edges.append((distY, Y[i+1][1], Y[i][1]))
+    edges.append((distZ, Z[i+1][1], Z[i][1]))
 
-while que:
-    dist, n1, n2 = hq.heappop(que)
+edges.sort()
 
+for dist, n1, n2 in edges:
     p1 = find(n1)
     p2 = find(n2)
 

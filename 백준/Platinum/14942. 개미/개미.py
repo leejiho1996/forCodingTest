ebdeb2e1d1e94ab0995ec97b2243fdd1a0ed6547
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-03-15 14:49:27 +0900

    [Platinum V] Title: 개미, Time: 2252 ms, Memory: 164408 KB -BaekjoonHub

--- 백준/Platinum/14942. 개미/개미.py
+++ 백준/Platinum/14942. 개미/개미.py
@@ -11,7 +11,7 @@ def dfs(n):
         if visited[i]:
             continue
         else:
-            dp[i][0] = n
+            parent[i][0] = n
             dist[i][0] = cost
             dfs(i)
 
@@ -20,7 +20,7 @@ LOG = int(math.log(n, 2))
 energy = []
 visited = [0] * (n+1)
 graph = [[] for _ in range(n+1)]
-dp = [[0] * (LOG+1) for _ in range(n+1)] # dp[i][j]는 노드i의 2^j번째 부모노드
+parent = [[0] * (LOG+1) for _ in range(n+1)] # parent[i][j]는 노드i의 2^j번째 부모노드
 dist = [[0] * (LOG+1) for _ in range(n+1)] # 2^j 부모노드까지 거리
 dist[1][0] = 1000001
 
@@ -36,8 +36,8 @@ dfs(1)
 
 for i in range(1, LOG+1):
     for j in range(1, n+1):
-        dp[j][i] = dp[dp[j][i-1]][i-1]
-        dist[j][i] = dist[j][i-1] + dist[dp[j][i-1]][i-1]
+        parent[j][i] = parent[parent[j][i-1]][i-1]
+        dist[j][i] = dist[j][i-1] + dist[parent[j][i-1]][i-1]
 
 for i in range(1, n+1):
     cur = i
@@ -46,7 +46,7 @@ for i in range(1, n+1):
     for j in range(LOG, -1, -1):
         if dist[cur][j] <= cur_energy:
             cur_energy -= dist[cur][j]
-            cur = dp[cur][j]
+            cur = parent[cur][j]
 
         if cur == 1:
             break

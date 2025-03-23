2eef1029b5e76094cbae79e7485113182ad6e4db
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-03-23 15:46:57 +0900

    [Gold II] Title: 마피아, Time: 1244 ms, Memory: 112508 KB -BaekjoonHub

---
+++ 백준/Gold/1079. 마피아/마피아.py
@@ -0,0 +1,56 @@
+# 마피아
+import sys
+input = sys.stdin.readline
+
+def solution():
+    
+    N = int(input())
+    guilty = list(map(int,input().split()))
+    graph = []
+    dead = [0] * N
+    result = 0
+
+    for i in range(N):
+        graph.append(list(map(int,input().split())))
+
+    E = int(input())
+
+    stack = [(N, 0, 0, guilty)]
+
+    while stack:
+        alive, dead, day, guilties = stack.pop()
+        
+        # 낮일 때
+        if alive % 2 == 1:
+            maxx = -1
+            max_idx = -1
+            
+            for i in range(N):
+                if dead & (1 << i):
+                    continue
+
+                if maxx < guilties[i]:
+                    maxx = guilties[i]
+                    max_idx = i
+            
+            if max_idx == E:
+                result = max(result, day)
+            else:
+                stack.append((alive-1, dead | (1 << max_idx), day, guilties.copy()))
+                
+        # 밤일 때
+        else:
+            for i in range(N):
+                if dead & (1 << i) or i == E:
+                    continue
+
+                tmp = guilties.copy()
+
+                for j in range(N):
+                    tmp[j] += graph[i][j]
+                    
+                stack.append((alive-1, dead | (1 << i) , day+1, tmp))
+
+    print(result)
+
+solution()

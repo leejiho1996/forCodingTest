6673d18f37dbc17baa513eeee073ba2e9e6636c0
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-03-30 16:11:36 +0900

    [Gold IV] Title: 약속장소 정하기 (Small), Time: 1912 ms, Memory: 39604 KB -BaekjoonHub

--- 백준/Gold/12441. 약속장소 정하기 （Small）/약속장소 정하기 （Small）.py
+++ 백준/Gold/12441. 약속장소 정하기 （Small）/약속장소 정하기 （Small）.py
@@ -3,6 +3,30 @@ import sys
 input = sys.stdin.readline
 import heapq as hq
 
+def dijkstra(n):
+    que = []
+    local_visited = [0] * (N+1)
+    dist = [float('inf')] * (N+1)
+    dist[start[n]] = 0    
+    hq.heappush(que, (0, start[n]))
+    cur_speed = speed[n]
+    
+    while que:
+        cost, cur = hq.heappop(que)
+
+        if local_visited[cur]:
+            continue
+
+        local_visited[cur] = 1
+        visited[cur] |= (1 << n)
+        total[cur] = max(total[cur], cost)
+
+        # 최소거리가 갱신 가능한 경우만 que에 넣는다
+        for ncity, ncost in graph[cur]:
+            if dist[ncity] > cost + ncost*cur_speed:
+                dist[ncity] = cost + ncost*cur_speed
+                hq.heappush(que, (dist[ncity], ncity))
+
 T = int(input())
 
 for i in range(T):
@@ -12,7 +36,7 @@ for i in range(T):
     speed = [0] * (N+1) # 친구 이동속도
     graph = [[] for _ in range(N+1)] # 도로 연결그래프
     visited = [1] * (N+1) # 비트마스킹으로 방문 확인
-    total = [0] * (N+1)
+    total = [0] * (N+1) # 도시별 최대 도달시간 저장
     
     for j in range(P):
         X, V = map(int,input().split())
@@ -24,7 +48,8 @@ for i in range(T):
 
         dist = roads[0]
         links = roads[1]
-    
+
+        # 주어진 도로들의 연결정보 저장
         for k in range(links):
             n1 = roads[2+k]
             for m in range(k+1, links):
@@ -33,27 +58,7 @@ for i in range(T):
                 graph[n2].append((n1, dist*(m-k)))
 
     for j in range(1, P+1):
-        que = []
-        hq.heappush(que, (0, start[j]))
-        cur_speed = speed[j]
-        visits = 0
-        
-        while que and visits < N:
-            dist, city = hq.heappop(que)
-            
-            if visited[city] & (1 << j):
-                continue
-            else:
-                visited[city] |= (1 << j)
-                total[city] = max(total[city], dist)
-                visits += 1
-
-            for n_city, n_dist in graph[city]:
-
-                if visited[n_city] & (1 << j):
-                    continue
-
-                hq.heappush(que, (n_dist*cur_speed + dist, n_city))
+        dijkstra(j)
             
     minn = float('inf')
     for j in range(1, N+1):
@@ -67,7 +72,3 @@ for i in range(T):
         print(f'Case #{i+1}: -1')
     else:
         print(f'Case #{i+1}: {minn}')
-
-            
-        
-    

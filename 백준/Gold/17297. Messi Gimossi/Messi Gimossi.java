a93507265c91839d3d44ce7849677c7e24d294df
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-04-03 22:31:27 +0900

    [Gold IV] Title: Messi Gimossi, Time: 100 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Gold/17297. Messi Gimossi/Messi Gimossi.java
+++ 백준/Gold/17297. Messi Gimossi/Messi Gimossi.java
@@ -12,38 +12,35 @@ public class Main {
 
         String base = "Messi Gimossi";
 
+        // 피보나치 수열로 M이 될때 까지 문자열 길이 저장
         int cur = 2;
         while (true) {
             long next = 1 + dp.get(cur-2) + dp.get(cur-1);
             dp.add(next);
 
-            if (next >= M) {
+            if (next >= M) { // 구하고자하는 M보다 큰 문자열이 완성되면 break
                 break;
             } else {
                 cur++;
             }
         }
 
+        // 기본 문자열인 Messi Gimossi가 될때 까지 문자열 소거
         while (M > 13) {
-            if (M > dp.get(cur-1)) {
-                M -= (dp.get(cur-1)+1);
+            if (M > dp.get(cur-1)) { // dp[cur-1] 보다 큰 경우
+                M -= (dp.get(cur-1)+1); // M은 dp[cur-1]에 공백 1 만큼 빼준다.
                 cur -= 2;
             } else {
+                // 위치만 dp[cur-1]로 바꿔준다
                 cur -= 1;
             }
 
-            if (M == 0) {
-                System.out.println("Messi Messi Gimossi");
-                System.exit(0);
-            }
         }
 
-        char result = base.charAt((int) M-1);
-
-        if (result == ' ') {
+        if (M == 0 || M == 6) { // 만약 M이 0이거나 6이라면 공백을 가르키는 것
             System.out.println("Messi Messi Gimossi");
         } else {
-            System.out.println(result);
+            System.out.println(base.charAt((int) M-1));
         }
     }
 }

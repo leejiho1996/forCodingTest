19fdf02cdc64f052c11a254caa3ec291aa8a9e9b
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-04-06 14:15:21 +0900

    [Gold V] Title: 배, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.py
+++ 백준/Gold/1092. 배/배.py
@@ -2,59 +2,31 @@
 import sys
 input = sys.stdin.readline
 
-def find(n):
-    if parent[n] < 0:
-        return parent[n]
-    
-    if parent[n] != n:
-        parent[n] = find(parent[n])
-
-    return parent[n]
-
 N = int(input())
 crane = list(map(int,input().split()))
 
 M = int(input())
 pears = list(map(int,input().split()))
 
-crane.sort()
-pears.sort()
+crane.sort(reverse = True)
+pears.sort(reverse = True)
 
-parent = [i for i in range(M)]
 cnt = 0
 time = 0
 
-if crane[-1] < pears[-1]:
+if crane[0] < pears[0]:
     print(-1)
     exit()
 
-while cnt < M:
+while pears:
     for i in range(N):
         cur = crane[i]
-        start = 0
-        end = M-1
-        
-        while start <= end:
-
-            mid = (start + end) // 2
-
-            if cur >= pears[mid]:
-                start = mid + 1
-            else:
-                end = mid - 1
-
-        p = find(start-1)
-
-        if p < 0 or pears[p] > cur:
-            continue
-        
-        if p == 0:
-            parent[p] = -1
-            cnt += 1
-        elif p > 0:
-            parent[p] = find(p-1)
-            cnt += 1
-            
+        if pears and cur < pears[-1]:
+                break
+        for v in pears:
+            if v <= cur:
+                pears.remove(v)
+                break
     time += 1
-
+    
 print(time)

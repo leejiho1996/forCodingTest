5ba863d529c836f768bcf183848f2a6304f21eba
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-04-20 13:42:27 +0900

    [Silver IV] Title: 점 숫자, Time: 132 ms, Memory: 54436 KB -BaekjoonHub

---
+++ 백준/Silver/8891. 점 숫자/점 숫자.py
@@ -0,0 +1,31 @@
+# 점숫자
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+dic1 = {}
+dic2 = {}
+cur = (1, 1)
+cnt = 1
+limit = 1
+
+for i in range(1, 100001):
+    dic1[cnt] = cur
+    dic2[cur] = cnt
+
+    if cur[1] == 1:
+        limit += 1
+        cur = (1, limit)
+    else:
+        cur = (cur[0]+1, cur[1]-1)
+
+    cnt += 1
+    
+for i in range(T):
+    a, b = map(int,input().split())
+    p1 = dic1[a]
+    p2 = dic1[b]
+
+    p = (p1[0]+p2[0], p1[1]+p2[1])
+    print(dic2[p])
+    

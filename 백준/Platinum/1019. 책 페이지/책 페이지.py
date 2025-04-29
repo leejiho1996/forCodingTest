5719cb500ba4752adf380e28c6760a50b1b7d269
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-04-30 00:13:01 +0900

    [Platinum V] Title: 책 페이지, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Platinum/1019. 책 페이지/책 페이지.py
+++ 백준/Platinum/1019. 책 페이지/책 페이지.py
@@ -5,10 +5,10 @@ input = sys.stdin.readline
 N = int(input())
 strN = str(N)
 length = len(strN)
-
-num_cnt = [0] * 10
-num_cnt[0] = 1
-one_to_nine = 0
+# num_cnt[i] => 10^i-1 까지 1-9가 모두 한번씩 나오는 횟수
+num_cnt = [0] * 11
+num_cnt[1] = 1
+one_to_nine = 0 # 1-9가 모두 한번씩 나오는 횟수
 result = [0] * 10
 
 if N < 10:
@@ -28,17 +28,18 @@ for i in range(1, length-1):
     else:
         zero += int(strN[:i]) * 10**(length-i-1)
 
-for i in range(1, 10):
-    num_cnt[i] = num_cnt[i-1] * 10 + 10**i
+# 10^(i-1)-1 까지 1-9가 모두 나오는 횟수를 계산
+for i in range(2, 11):
+    num_cnt[i] = num_cnt[i-1] * 10 + 10**(i-1)
     
 for i in range(length-1):
     cur = int(strN[i])
-
+    # 현재 숫자가 0인 경우는 continue
     if cur == 0:
         continue
-    
+    # 현재 숫자는 뒤에 숫자+1 만큼 반복해서 나온다
     result[cur] += int(strN[i+1:])+1
-    one_to_nine += cur * num_cnt[length-i-2]
+    one_to_nine += cur * num_cnt[length-i-1]
     
     for j in range(1, cur):
         result[j] += 10**(length-i-1)

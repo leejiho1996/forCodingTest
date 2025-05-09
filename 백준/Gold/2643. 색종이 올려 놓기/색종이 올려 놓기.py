86b4f62535f7185a52b1a7e9eafcda5645c554fc
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-05-09 13:38:46 +0900

    [Gold IV] Title: 색종이 올려 놓기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2643. 색종이 올려 놓기/색종이 올려 놓기.py
@@ -0,0 +1,47 @@
+# 색종이 올려 놓기
+import sys
+input = sys.stdin.readline
+
+def dfs(n):
+    ret = 1
+    
+    if dp[n] > 0:
+        return dp[n]
+
+    ca, cb = papers[n]
+
+    for i in range(N):
+        if visited[i]:
+            continue
+
+        na, nb = papers[i]
+
+        if (ca < na or cb < nb) and (ca < nb or cb < na):
+            continue
+        
+        visited[i] = 1
+        ret = max(ret, dfs(i)+1)
+        visited[i] = 0
+
+    dp[n] = ret
+    return dp[n]
+
+N = int(input())
+papers = []
+dp = [0] * N
+visited = [0]*(N)
+result = 0
+
+for i in range(N):
+    a, b = map(int,input().split())
+    papers.append((a,b))
+
+for i in range(N):
+    visited[i] = 1
+    dfs(i)
+    visited[i] = 0
+
+for i in range(N):
+    result = max(result, dp[i])
+
+print(result)

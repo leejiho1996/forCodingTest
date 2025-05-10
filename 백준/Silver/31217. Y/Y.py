69c0425e2fb3e733afd6aa3be27764afd46298c7
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-05-10 14:49:23 +0900

    [Silver III] Title: Y, Time: 224 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/31217. Y/Y.py
+++ 백준/Silver/31217. Y/Y.py
@@ -2,19 +2,20 @@
 import sys
 input = sys.stdin.readline
 
+# n을 루트로하는 Y를 계산 ((n과 연결된 노드 크기) C 3)
 def cal(n):
-    size = len(graph[n])
+    size = link[n]
     return size * (size-1) * (size-2) // 6 % MOD
 
 N, M = map(int,input().split())
-graph = [[] for _ in range(N+1)]
+link = [0] * (N+1)
 MOD = 1_000_000_007
 
+# 연결정보를 입력받고 저장
 for i in range(M):
     a, b = map(int,input().split())
-
-    graph[a].append(b)
-    graph[b].append(a)
+    link[a] += 1
+    link[b] += 1
 
 result = 0
 for i in range(1, N+1):

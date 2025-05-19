08b8ae9882fa740860bb8be9c251a675f250b8a5
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-05-19 12:43:23 +0900

    [Platinum V] Title: 공장, Time: 756 ms, Memory: 116940 KB -BaekjoonHub

--- 백준/Platinum/7578. 공장/공장.java
+++ 백준/Platinum/7578. 공장/공장.java
@@ -2,13 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N;
     static long[] segTree;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
         int[] order = new int[1000001];
         int[] up  = new int[N];
@@ -28,26 +29,29 @@ public class Main {
         }
 
         for (int i = 0; i < N; i++) {
-            result += query(0, N, order[up[i]], N, 0);
+            // 현재 이어지는 지점보다 오른쪽에 위치한 지점 갯수 확인
+            result += query(0, N, order[up[i]], 0);
+            // 세그먼트 트리 업데이트
             update(0, N, order[up[i]], 0);
         }
 
         System.out.println(result);
     }
 
-    static long query(int left, int right, int leftNode, int rightNode, int node) {
+    // leftNode와 rightNode 사이의
+    static long query(int left, int right, int start,  int node) {
 
-        if (right < leftNode || left > rightNode) {
+        if (right < start || left > N) {
             return 0;
         }
 
-        if (left >= leftNode && rightNode >= right) {
+        if (left >= start && N >= right) {
             return segTree[node];
         }
         int mid = (left + right) / 2;
 
-        long leftQuery = query(left, mid, leftNode, rightNode, node*2+1);
-        long rightQuery = query(mid + 1, right, leftNode, rightNode, node*2+2);
+        long leftQuery = query(left, mid, start, node*2+1);
+        long rightQuery = query(mid + 1, right, start,node*2+2);
 
         return leftQuery + rightQuery;
     }

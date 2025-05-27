40b9783baa47933309282100e31f3a65bb0b53ae
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-05-27 15:59:57 +0900

    [Gold IV] Title: 램프, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1034. 램프/램프.py
+++ 백준/Gold/1034. 램프/램프.py
@@ -2,56 +2,23 @@
 import sys
 input = sys.stdin.readline
 
-def switch(j):
-    for i in range(N):
-        if ramps[i][j] == '0':
-            ramps[i][j] = '1'
-        else:
-            ramps[i][j] = '0'
-            
-#-- 입력
 N, M = map(int,input().split())
-ramps = []
+dic = {}
 result = 0
 
 for i in range(N):
-    row = list(input().rstrip())
-    ramps.append(row)
+    row = input().rstrip()
+
+    if row not in dic:
+        dic[row] = 1
+    else:
+        dic[row] += 1
 
 K = int(input())
-#-- 입력 끝
 
-for i in range(N):
-    switches = []
+for row in dic.keys():
 
-    for j in range(M):
-        if ramps[i][j] == '0' and K:
-             switch(j)
-             switches.append(j)
-             K -= 1
-
-    if K % 2 == 1:
-        K += len(switches)
-
-        for s in switches:
-            switch(s)
-
-        continue
-    
-    total = 0
-    for j in range(N):
-        cnt = 0
-        for k in range(M):
-            if ramps[j][k] == '1':
-                cnt += 1
-
-        if cnt == M:
-            total += 1
-
-    result = max(result, total)
-
-    K += len(switches)
-    for s in switches:
-        switch(s)
-
+    if row.count('0') <= K and (K - row.count('0')) % 2 == 0:
+        result = max(result, dic[row])
+        
 print(result)

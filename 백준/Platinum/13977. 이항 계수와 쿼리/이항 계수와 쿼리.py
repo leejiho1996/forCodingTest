1cd8a14144666341832590a7de9d0c321f5e3d91
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-05-28 15:54:32 +0900

    [Platinum V] Title: 이항 계수와 쿼리, Time: 392 ms, Memory: 146480 KB -BaekjoonHub

--- 백준/Platinum/13977. 이항 계수와 쿼리/이항 계수와 쿼리.py
+++ 백준/Platinum/13977. 이항 계수와 쿼리/이항 계수와 쿼리.py
@@ -2,10 +2,12 @@
 import sys
 input = sys.stdin.readline
 
+# 4,000,000까지의 팩토리얼 저장
 def init():
     for i in range(1, 4_000_001):
         dp[i] = (dp[i-1] * i) % MOD 
 
+# 분할 정복으로 MOD-2제곱 계산
 def cal(num, power):
 
     if power == 1:
@@ -18,7 +20,7 @@ def cal(num, power):
     else:
         return res* res % MOD
 
-dp = [0] * (4_000_001 * 4)
+dp = [0] * (4_000_001)
 dp[0] = 1
 MOD = 1_000_000_007
 
@@ -28,5 +30,10 @@ N = int(input())
 
 for i in range(N):
     A, B = map(int,input().split())
+    # 페르마의 소정리
+    # 1. a^P = a (mod P)
+    # 2. a^(p-1) = 1 (mod P)
+    # 3. a^(P-2) = 1/a (mod P)
+    # N!/K!(N-K)! -> N! * (K!(N-K!))^-1
     print(dp[A] * cal(dp[B]*dp[A-B], MOD-2) % MOD)
 

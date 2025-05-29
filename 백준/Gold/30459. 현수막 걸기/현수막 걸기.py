f2426ab22defda61c9d63b6b5c5e520f6d6b2bbe
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-05-29 14:19:38 +0900

    [Gold V] Title: 현수막 걸기, Time: 344 ms, Memory: 114896 KB -BaekjoonHub

---
+++ 백준/Gold/30459. 현수막 걸기/현수막 걸기.py
@@ -0,0 +1,33 @@
+# 현수막 걸기
+import sys
+input = sys.stdin.readline
+
+N, K, R = map(int,input().split())
+stakes = list(map(int,input().split()))
+flagpoles = list(map(int,input().split()))
+
+stakes.sort()
+flagpoles.sort()
+
+result = -1
+
+for i in range(N):
+    for j in range(i+1, N):
+        width = stakes[j] - stakes[i]
+        target = R / width * 2
+        
+        start = 0
+        end = K-1
+
+        while start <= end:
+            mid = (start + end) // 2
+
+            if flagpoles[mid] <= target:
+                start = mid+1
+            else:
+                end = mid-1
+
+        if flagpoles[start-1] <= target:
+            result = max(result, width * flagpoles[start-1] / 2)
+            
+print(result)

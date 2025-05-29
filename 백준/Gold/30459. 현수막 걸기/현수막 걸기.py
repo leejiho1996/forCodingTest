99185571e336ee6f90d93835937a8d0203ebaabe
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-05-29 17:03:03 +0900

    [Gold V] Title: 현수막 걸기, Time: 128 ms, Memory: 115668 KB -BaekjoonHub

--- 백준/Gold/30459. 현수막 걸기/현수막 걸기.py
+++ 백준/Gold/30459. 현수막 걸기/현수막 걸기.py
@@ -16,16 +16,22 @@ for i in range(N):
     for j in range(i+1, N):
         width = stakes[j] - stakes[i]
 
+        # 한번 확인한 밑변은 패스
         if visited[width]:
             continue
         else:
             visited[width] = 1
-            
+
+        # 현재 밑변에 가능한 가장 큰 높이    
         target = R / width * 2
+
+        if flagpoles[0] > target:
+            continue
         
         start = 0
         end = K-1
 
+        # 이분 탐색으로 현재 가능한 가장 큰 높이를 찾는다
         while start <= end:
             mid = (start + end) // 2
 
@@ -34,8 +40,9 @@ for i in range(N):
             else:
                 end = mid-1
 
+        # 해당 높이가 target보다 같거나 작으면 result 갱신
         if flagpoles[start-1] <= target:
             result = max(result, width * flagpoles[start-1] / 2)
-           
+
 print(result)
     

46b7a4d0834ac6a8b7a463da1c1e9a03a7135451
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-05-30 13:17:02 +0900

    [Gold IV] Title: 가운데에서 만나기, Time: 332 ms, Memory: 27920 KB -BaekjoonHub

---
+++ 백준/Gold/21940. 가운데에서 만나기/가운데에서 만나기.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] graph = new int[N+1][N+1];
+        int result = 10000000;
+
+        for (int i = 1; i < N+1; i++) {
+            Arrays.fill(graph[i], 10000000);
+            graph[i][i] = 0;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int T = Integer.parseInt(st.nextToken());
+
+            graph[A][B] = T;
+        }
+
+        // 친구들의 시작노드 입력
+        int K = Integer.parseInt(br.readLine());
+        int[] friends = new int[K];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < K; i++) {
+            friends[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 플로이드 워셜로 각 노드별 최단거리 계산
+        for (int k = 1; k < N+1; k++) {
+            for (int i = 1; i < N+1; i++) {
+                for (int j = 1; j < N+1; j++) {
+                    if (graph[i][j] > graph[i][k] + graph[k][j]) {
+                        graph[i][j] = graph[i][k] + graph[k][j];
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < N+1; i++) {
+            int curMax = 0;
+
+            for (int j : friends) {
+                curMax = Math.max(curMax, graph[i][j] + graph[j][i]);
+            }
+
+            if (curMax < result) {
+                result = curMax;
+                sb = new StringBuilder();
+                sb.append(i).append(" ");
+            } else if (curMax == result) {
+                sb.append(i).append(" ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}

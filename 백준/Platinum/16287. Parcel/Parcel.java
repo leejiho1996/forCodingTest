c4994caabfb395b46f50aa5693475dc960a18e00
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-06-04 17:56:09 +0900

    [Platinum V] Title: Parcel, Time: 296 ms, Memory: 24936 KB -BaekjoonHub

--- 백준/Platinum/16287. Parcel/Parcel.java
+++ 백준/Platinum/16287. Parcel/Parcel.java
@@ -2,61 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    // Parcel
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int W = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        int[] A = new int[N];
+        int[] nums = new int[N];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[][] twoSum = new int[W + 1][2];
-        for (int i = 0; i <= W; i++) {
-            twoSum[i][0] = -1;
-            twoSum[i][1] = -1;
-        }
+        int[] sumFirst = new int [W+1];
+        int[] sumSecond = new int [W+1];
+
+        Arrays.fill(sumFirst, -1);
+        Arrays.fill(sumSecond, -1);
 
         for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                int num = A[i] + A[j];
+            for (int j = i+1; j < N; j++) {
+                int sum = nums[i] + nums[j];
 
-                if (num >= W + 1) {
+                if (sum >= W+1) {
                     continue;
                 }
 
-                // 처음 만들어지는 순서쌍만 저장
-                if (twoSum[num][0] == -1) {
-                    twoSum[num][0] = i;
-                    twoSum[num][1] = j;
+                if (sumFirst[sum] == -1 && sumSecond[sum] == -1) {
+                    sumFirst[sum] = i;
+                    sumSecond[sum] = j;
                 }
             }
         }
 
         for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                // W를 만들기 위해 필요한 숫자
-                int need = W - (A[i] + A[j]);
+            for (int j = i+1; j < N; j++) {
+                int need = W - (nums[i] + nums[j]);
 
-                if (need <= 0) { // 0이하의 수 체크
+                if (need <= 0) {
                     continue;
                 }
 
-                // need를 만들 수 있는지 체크
-                if (twoSum[need][0] == -1) {
+                if (sumFirst[need] == -1 && sumSecond[need] == -1) {
                     continue;
                 }
 
-                int x = twoSum[need][0];
-                int y = twoSum[need][1];
+                int first = sumFirst[need];
+                int second = sumSecond[need];
 
-                // 네개의 인덱스가 모두 다른지 확인
-                if (i != x && i != y && j != x && j != y) {
+                if (i != first && j != second && i != second && j != first) {
                     System.out.println("YES");
-                    return;
+                    System.exit(0);
                 }
             }
         }

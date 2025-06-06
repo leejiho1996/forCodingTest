94ba5698ad2acb47d18060ccd6635710857dd8bc
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-06-06 18:43:14 +0900

    [Gold III] Title: 카드 게임, Time: 2276 ms, Memory: 220444 KB -BaekjoonHub

---
+++ 백준/Gold/11062. 카드 게임/카드 게임.py
@@ -0,0 +1,32 @@
+# 카드 게임
+import sys
+input = sys.stdin.readline
+
+def solve(left, right, turn):
+
+    if left > right:
+        return 0
+
+    if turn == 1:
+       return min(solve(left+1, right, 1-turn),
+                  solve(left, right-1, 1-turn))
+    
+    if dp[left][right] != -1:
+        return dp[left][right]
+    else:
+        dp[left][right] = 0
+
+    dp[left][right] = max(solve(left+1, right, 1-turn) + cards[left],
+                          solve(left, right-1, 1-turn) + cards[right])
+
+    return dp[left][right]
+
+T = int(input())
+
+for i in range(T):
+    N = int(input())
+    cards = list(map(int,input().split()))
+    dp = [[-1] * N for _ in range(N)]
+
+    print(solve(0, N-1, 0))
+    

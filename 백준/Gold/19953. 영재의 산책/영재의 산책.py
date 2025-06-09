eeb6096a099d49becca45db9998c49fee8663f86
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-06-09 23:57:00 +0900

    [Gold IV] Title: 영재의 산책, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/19953. 영재의 산책/영재의 산책.py
@@ -0,0 +1,52 @@
+# 영재의 산책
+import sys
+input = sys.stdin.readline
+
+V, M, T = map(int,input().split())
+
+T -= 1
+X = 0
+Y = V # 처음은 북쪽으로 이동
+direc = 0 # 처음 방향은 북
+
+# 북 0 동 1 남 2 서 3
+pos = {0 : (0, 1), 1 : (1, 0), 2: (0, -1), 3 : (-1, 0)}
+sett = set()
+cycle = []
+
+while True:
+    V = (V * M) % 10
+
+    if V in sett:
+        break
+    else:
+        cycle.append(V)
+        sett.add(V)
+
+# 한바퀴 돌았을 때 이동거리 계산
+dx = 0
+dy = 0
+
+for i in range(4):
+    direc = (direc+1) % 4
+
+    dx += pos[direc][0] * cycle[i % len(cycle)]
+    dy += pos[direc][1] * cycle[i % len(cycle)]
+    
+X += dx * (T // 4)
+Y += dy * (T // 4)
+
+# 남은 횟수 계산
+dx = 0
+dy = 0
+
+for i in range(T % 4):
+    direc = (direc+1) % 4
+
+    dx += pos[direc][0] * cycle[i % len(cycle)]
+    dy += pos[direc][1] * cycle[i % len(cycle)]
+
+print(X + dx, Y + dy)
+
+
+    

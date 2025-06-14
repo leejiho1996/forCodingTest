286d77a82c8a9dd7e8bf8883abf9d4a2fe9046a4
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-06-14 16:35:41 +0900

    [Gold III] Title: 간단한 순열 문제, Time: 184 ms, Memory: 53288 KB -BaekjoonHub

---
+++ 백준/Gold/31858. 간단한 순열 문제/간단한 순열 문제.py
@@ -0,0 +1,25 @@
+# 간단한 순열 문제
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+nums = list(map(int,input().split()))
+result = 0
+
+stack = []
+
+for i in range(N):
+
+    while stack and stack[-1] < nums[i]:
+
+        idx = stack.pop()
+        result += 1
+
+        if stack:
+            result += 1
+
+    stack.append(nums[i])
+
+result += len(stack) - 1
+
+print(result)

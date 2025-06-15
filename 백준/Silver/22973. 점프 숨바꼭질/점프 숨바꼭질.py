ffc528ad62576052e578704a9481e4072a12e809
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-06-15 23:20:55 +0900

    [Silver I] Title: 점프 숨바꼭질, Time: 36 ms, Memory: 34536 KB, Score: 125 point -BaekjoonHub

--- 백준/Silver/22973. 점프 숨바꼭질/점프 숨바꼭질.py
+++ 백준/Silver/22973. 점프 숨바꼭질/점프 숨바꼭질.py
@@ -1,30 +1,28 @@
 # 점프 숨바꼭질
 import sys
 input = sys.stdin.readline
-from collections import deque
+import math
 
 K = int(input())
-dic = {}
 
-que = deque([(0, 1, 0)])
-dic[0] = 0
-LIMIT = 10**12
-
-while que:
-    cur, jump, time = que.popleft()
+if K == 0:
+    print(0)
+    exit()
     
-    minus = cur - jump
-    plus = cur + jump
-    
-    if minus not in dic and minus >= -LIMIT:
-        dic[minus] = time + 1
-        que.append((minus, jump * 2, time+1))
-        
-    if plus not in dic and plus <= LIMIT:
-        dic[plus] = time + 1
-        que.append((plus, jump * 2, time+1))
+jump = 2**int(math.log(abs(K), 2))
+cnt = 0
 
-if K in dic:
-    print(dic[K])
+while jump:
+    if K < 0:
+        K += jump
+    else:
+        K -= jump
+
+    jump //= 2
+    cnt += 1
+    
+if K == 0:
+    print(cnt)
 else:
     print(-1)
+

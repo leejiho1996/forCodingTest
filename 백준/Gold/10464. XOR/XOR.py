41a6aa9e3c3572a42d660686413df7e9fc874a7e
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-06-16 00:07:37 +0900

    [Gold IV] Title: XOR, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/10464. XOR/XOR.py
+++ 백준/Gold/10464. XOR/XOR.py
@@ -1,45 +1,22 @@
-# XOR
+# XOR 초간단
 import sys
 input = sys.stdin.readline
-
-def cal(num, lis):
-
-    div = 1
-    while div <= num:
-        cnt = 0
-
-        start = num - (div-1)
-        times = start // div
-        resi = start % div
-
-        cnt += (times+1) // 2 * div
-
-        if times % 2 == 0:
-            cnt += resi
-
-        lis.append(cnt)
-        
-        div *= 2
 
 T = int(input())
 
 for i in range(T):
     S, F = map(int,input().split())
 
-    listS = []
-    listF = []
+    S -= 1
+    
+    resultF = 0
 
-    cal(S-1, listS)
-    cal(F, listF)
+    for j in range(F- (F % 4), F+1):
+        resultF ^= j
 
-    for i in range(len(listS)):
-        listF[i] -= listS[i]
+    resultS = 0
+    
+    for j in range(S - (S % 4), S+1):
+        resultS ^= j
 
-    result = ""
-    for i in listF:
-        if i % 2 == 1:
-            result += "1"
-        else:
-            result += "0"
-
-    print(int(result[::-1], 2))
+    print(resultS ^ resultF)

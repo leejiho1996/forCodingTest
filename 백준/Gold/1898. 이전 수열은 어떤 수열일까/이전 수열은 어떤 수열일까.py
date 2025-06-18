150330a2afae4b8f561d2d2f8c637d65d4632ffd
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-06-18 11:28:22 +0900

    [Gold II] Title: 이전 수열은 어떤 수열일까, Time: 104 ms, Memory: 36308 KB -BaekjoonHub

--- 백준/Gold/1898. 이전 수열은 어떤 수열일까/이전 수열은 어떤 수열일까.py
+++ 백준/Gold/1898. 이전 수열은 어떤 수열일까/이전 수열은 어떤 수열일까.py
@@ -7,6 +7,7 @@ origin = []
 idx = [-1] * (N+1)
 result = [0] * N
 
+# 숫자를 입력받고 인덱스를 저장
 for i in range(N):
     num = int(input())
     origin.append(num)
@@ -14,13 +15,13 @@ for i in range(N):
 
 for i in range(N):
     cur = origin[i]
-
+    # 이미 다른 숫자와 바꾼 숫자라면 패스
     if idx[cur] == -1:
         continue
     
-    m1 = idx[cur-1]
+    m1 = idx[cur-1] # 현재 숫자보다 1작은 숫자의 인덱스
 
-    if m1 == 0 or m1 < idx[cur]:
+    if m1 == -1 or m1 < idx[cur]:
         result[i] = cur
     else:
         result[i] = cur-1

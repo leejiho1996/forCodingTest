3b51a4e7a320f8f367437bee3ed6c7a19606c58e
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-06-19 18:39:52 +0900

    [Gold III] Title: 총깡 총깡, Time: 80 ms, Memory: 39604 KB -BaekjoonHub

--- 백준/Gold/14618. 총깡 총깡/총깡 총깡.py
+++ 백준/Gold/14618. 총깡 총깡/총깡 총깡.py
@@ -5,7 +5,8 @@ import heapq as hq
 
 N, M = map(int,input().split())
 graph = [[] for _ in range(N+1)]
-visited = [float('inf')] * (N+1)
+LIMIT = 500001
+visited = [LIMIT] * (N+1)
 
 J = int(input())
 K = int(input())
@@ -20,13 +21,15 @@ for i in range(M):
 
 que = []
 
+# 진서를 시작점으로 주변 도시를 힙큐에 담는다
 for city, cost in graph[J]:
     hq.heappush(que, (cost, city))
     visited[city] = cost
 
-minA = float('inf')
-minB = float('inf')
+minA = LIMIT
+minB = LIMIT
 
+# 모든 도시까지의 최단거리를 다익스트라로 탐색
 while que:
     cost, city = hq.heappop(que)
 
@@ -35,10 +38,13 @@ while que:
     
     if city in A:
         minA = min(minA, cost)
-
+    
     if city in B:
         minB = min(minB, cost)
 
+    if minA != LIMIT and minB != LIMIT:
+        break
+    
     for ncity, ncost in graph[city]:
         if visited[ncity] <= cost + ncost:
             continue
@@ -46,9 +52,10 @@ while que:
         hq.heappush(que, (cost + ncost, ncity))
         visited[ncity] = cost + ncost
 
-if minA == float('inf') and minB == float('inf'):
+# 둘다 도달하지 못하면 -1 출력
+if minA == LIMIT and minB == LIMIT:
     print(-1)
-elif minA <= minB:
+elif minA <= minB: # A, B 둘다 같은 경우 A를 출력
     print("A")
     print(minA)
 else:

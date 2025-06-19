7fcf024c824bf50721a7be5315422f9c4815ab65
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-06-19 18:25:03 +0900

    [Gold III] Title: 총깡 총깡, Time: 316 ms, Memory: 26020 KB -BaekjoonHub

---
+++ 백준/Gold/14618. 총깡 총깡/총깡 총깡.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        final int LIMIT = 500001;
+
+        int[] visited = new int[N+1];
+        ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
+
+        for (int i = 0; i < N+1; i++) {
+            graph.add(new ArrayList<>());
+            visited[i] = LIMIT;
+        }
+
+        int J = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+        HashSet<Integer> A = new HashSet<>();
+        HashSet<Integer> B = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < K; i++) {
+            A.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < K; i++) {
+            B.add(Integer.parseInt(st.nextToken()));
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            graph.get(s).add(new int[] {t, c});
+            graph.get(t).add(new int[] {s, c});
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        for (int[] tmp : graph.get(J)) {
+            int city = tmp[0];
+            int cost = tmp[1];
+
+            pq.offer(new int[] {cost, city});
+            visited[city] = cost;
+        }
+
+
+        int minA = LIMIT;
+        int minB = LIMIT;
+
+        while (!pq.isEmpty()) {
+            int[] tmp = pq.poll();
+            int cost = tmp[0];
+            int city = tmp[1];
+
+            if (visited[city] < cost) {
+                continue;
+            }
+
+            if (A.contains(city)) {
+                minA = Math.min(minA, cost);
+            }
+
+            if (B.contains(city)) {
+                minB = Math.min(minB, cost);
+            }
+
+            for (int[] next : graph.get(city)) {
+                int nextCity = next[0];
+                int nextCost = next[1];
+
+                if (visited[nextCity] <= cost + nextCost) {
+                    continue;
+                }
+
+                pq.offer(new int[] {cost + nextCost, nextCity});
+                visited[nextCity] = cost + nextCost;
+            }
+        }
+
+        if (minA == LIMIT && minB == LIMIT) {
+            System.out.println(-1);
+        } else if (minA <= minB) {
+            System.out.println("A");
+            System.out.println(minA);
+        } else {
+            System.out.println("B");
+            System.out.println(minB);
+        }
+    }
+}

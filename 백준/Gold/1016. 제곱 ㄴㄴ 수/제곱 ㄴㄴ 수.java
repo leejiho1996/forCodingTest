9ae268c3f9f45373a518cf393fb9a9ee9f937c11
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-06-23 02:32:46 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 164 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -8,29 +8,31 @@ public class Main {
 
         long min = Long.parseLong(st.nextToken());
         long max = Long.parseLong(st.nextToken());
-        int cnt = (int) (max - min  + 1);
+        int cnt = (int) (max - min  + 1); // # 숫자의 갯수
         int result = 0;
         boolean[] visited = new boolean[cnt];
 
-        int cur = 2;
-        long sqr = 4;
+        long cur = 2; // 2부터 시작
 
-        while (sqr <= max) {
+        while (cur*cur <= max) { // 현재의 제곱수가 최댓값보다 작을때까지 반복
 
-            long start = 0;
-            if (min % sqr != 0) {
+            long sqr = cur * cur;
+
+            long start = 0; //  최솟값이 현재 제곱수로 나누어떨어지면 최솟값(0번 인덱스)부터 시작
+            if (min % sqr != 0) { // 그렇지 않다면 시작 인덱스 계산
                 start = (sqr * (min / sqr + 1) - min);
             }
 
+            // 시작인덱스부터 현재 제곱수 간격으로 체크해준다
             for (long i = start; i < cnt; i+=sqr) {
                 visited[(int) i] = true;
             }
 
             cur++;
-            sqr = (long) cur * cur;
 
         }
 
+        // 제곱 ㄴㄴ수 카운팅
         for (int i = 0; i < cnt; i++) {
             if (!visited[i]) result++;
         }

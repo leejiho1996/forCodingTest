7a0f8cc9a4d8ab098a492346ad7139c6e7808e4f
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-06-23 02:06:17 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 1092 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -3,11 +3,9 @@ import sys
 input = sys.stdin.readline
 
 minn, maxx = map(int,input().split())
-dic = {}
 result = 0
-
-for i in range(minn, maxx+1):
-    dic[i] = 0
+cnt = maxx - minn + 1
+visited = [0] * cnt
 
 cur = 2
 while cur**2 <= maxx:
@@ -15,17 +13,17 @@ while cur**2 <= maxx:
     sqr = cur**2
 
     if minn % sqr == 0:
-        start = minn
+        start = 0
     else:
-        start = sqr * (minn // sqr + 1)
+        start = sqr * (minn // sqr + 1) - minn
 
-    for i in range(start, maxx+1, sqr):
-        dic[i] = 1
+    for i in range(start, cnt, sqr):
+        visited[i] = 1
 
     cur += 1
 
-for i in range(minn, maxx+1):
-    if dic[i] == 0:
+for i in range(cnt):
+    if visited[i] == 0:
         result += 1
     
 print(result)

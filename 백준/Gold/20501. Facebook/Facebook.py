e79cf0765f4129a33d2cc6f46eac5b5012e7f164
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-06-29 00:00:42 +0900

    [Gold II] Title: Facebook, Time: 4028 ms, Memory: 35700 KB -BaekjoonHub

--- 백준/Gold/20501. Facebook/Facebook.py
+++ 백준/Gold/20501. Facebook/Facebook.py
@@ -1,35 +1,18 @@
-#Facebook
+#Facebook2
 import sys
 input = sys.stdin.readline
 
 N = int(input())
-links = [[] for _ in range(N)]
-bitCnt = [0] * ((1 << 20) + 1)
+links = []
 
 for i in range(N):
-    link = input().rstrip()
+    links.append(input().rstrip())
 
-    start = 0
-    end = 20
-    
-    while start < N:
-        links[i].append(int(link[start:end], 2))
-        start = end
-        end += 20
-    
-for i in range((1 << 20) + 1):
-    bitCnt[i] = i.bit_count()
-
+# Q입력
 Q = int(input())
 
 for i in range(Q):
     a, b = map(int,input().split())
-    cnt = 0
-    
-    for j in range(len(links[0])):
-        cnt += bitCnt[links[a-1][j] & links[b-1][j]]
 
-    print(cnt)
-        
-
+    print((int(links[a-1], 2) & int(links[b-1], 2)).bit_count())
         

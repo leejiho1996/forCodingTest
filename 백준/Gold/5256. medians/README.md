acb8654945f1ec3778a8c0545dd2262fe6d25b8f
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-07-06 19:18:12 +0900

    [Gold I] Title: medians, Time: 772 ms, Memory: 104184 KB -BaekjoonHub

---
+++ 백준/Gold/5256. medians/README.md
@@ -0,0 +1,34 @@
+# [Gold I] medians - 5256 
+
+[문제 링크](https://www.acmicpc.net/problem/5256) 
+
+### 성능 요약
+
+메모리: 104184 KB, 시간: 772 ms
+
+### 분류
+
+애드 혹, 그리디 알고리즘
+
+### 제출 일자
+
+2025년 7월 6일 19:17:59
+
+### 문제 설명
+
+<p>Let A be a permutation of 1, 2, 3 ..., 2*N - 1.</p>
+
+<p>We define the prefix medians of A as an array B with N elements: where B[i] is the median of A[1], A[2], ..., A[2*i-1].</p>
+
+<p>Note: The median of a list of M numbers (where M is odd) can be found by sorting the numbers and picking the middle one.</p>
+
+<p>You are given N and the array B. You are asked to determine a permutation A whose prefix medians are precisely B.</p>
+
+### 입력 
+
+ <p>The input file contains 2 lines. The first line contains one integer, N. The second line describes B: N integers, separated by space.</p>
+
+### 출력 
+
+ <p>The output file should contain A: one line with 2*N-1 integers separated by space. If there are multiple permutations A leading to the same input array B, you may output any one. In all test data, there will always be at least one solution.</p>
+
---
+++ 백준/Gold/5256. medians/medians.py
@@ -0,0 +1,59 @@
+# medians
+import sys
+input = sys.stdin.readline
+import heapq as hq
+
+def popQue(limit, que):
+
+    cnt = 0
+    
+    while cnt < limit:
+        num, pri = hq.heappop(que)
+        num = abs(num)
+        
+        if visited[num]:
+            continue
+        else:
+            visited[num] = 1
+            result.append(num)
+
+        cnt += 1
+
+N = int(input())
+B = list(map(int,input().split()))
+visited = [0] * (2*N)
+visited[B[0]] = 1
+
+result = [B[0]]
+
+minQue = []
+maxQue = []
+
+for i in range(N):
+    hq.heappush(minQue, (B[i], -1))
+    hq.heappush(maxQue, (-B[i], -1))
+
+for i in range(2*N-1):
+    hq.heappush(minQue, (i+1, 0))
+    hq.heappush(maxQue, (-(i+1), 0))
+    
+for i in range(1, N):
+    cur = B[i]
+
+    if visited[cur]:
+        limit = 2
+    else:
+        limit = 1
+        result.append(cur)
+        visited[cur] = 1
+        
+    if cur < B[i-1]:
+        popQue(limit, minQue)        
+    elif cur > B[i-1]:
+        popQue(limit, maxQue)
+    else:
+        popQue(1, minQue)
+        popQue(1, maxQue)
+
+print(*result)
+        

b31d80c4d58b924f345ca596a85799a4bc877bc3
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-07-08 15:48:41 +0900

    [Gold I] Title: 수영장 만들기, Time: 72 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/1113. 수영장 만들기/수영장 만들기.py
+++ 백준/Gold/1113. 수영장 만들기/수영장 만들기.py
@@ -1,56 +1,55 @@
-from collections import deque
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-def bfs(i,j):
-    
-    cur = graph[i][j]
-    maxx = 10
-    que = deque([(i,j)])
-    visits = [(i,j)]
-    
-    visited = [[0] * (M) for _ in range(N)]
-    visited[i][j] = 1
+def bfs(r, c, height):
 
+    visited[r][c] = 1
+    que = deque([(r, c)])
+    check = True
     ret = 0
     
     while que:
         r, c = que.popleft()
+        ret += height - graph[r][c]
+        graph[r][c] = height
 
         for dr, dc in direc:
-            nr = r + dr
-            nc = c + dc
+            nr, nc = r + dr, c + dc
 
             if nr < 0 or nc < 0 or nr >= N or nc >= M:
-                return 0
+                check = False
+                continue
 
             if visited[nr][nc]:
                 continue
 
-            if graph[nr][nc] > cur:
-                maxx = min(maxx, graph[nr][nc])    
-            else:
+            if graph[nr][nc] < height:
                 que.append((nr, nc))
-                visits.append((nr, nc))
-                visited[nr][nc] = 1
-
-    for r, c in visits:
-        ret += maxx - graph[r][c] 
-        graph[r][c] = maxx
-
-    return ret
+                visited[nr][nc] = True
+    
+    if check:
+        return ret
+    else:
+        return 0
 
 N, M = map(int, input().split())
 graph = []
-direc = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+direc = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 result = 0
 
 for i in range(N):
     row = list(input().rstrip())
     graph.append(list(map(int, row)))
     
-for i in range(1, N-1):
-    for j in range(1, M-1):
-        result += bfs(i,j)
+for k in range(2, 10):
+    visited = [[0] * M for _ in range(N)]
+    for i in range(N):
+        for j in range(M):
+            if visited[i][j]:
+                continue
+            
+            if graph[i][j] < k:
+                result += bfs(i, j, k)
 
 print(result)

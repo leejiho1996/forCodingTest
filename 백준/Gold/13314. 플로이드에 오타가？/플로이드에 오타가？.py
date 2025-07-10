727aadaf9f7b4c05d67a9f354223a22eca866844
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-07-10 12:01:33 +0900

    [Gold III] Title: 플로이드에 오타가?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/13314. 플로이드에 오타가？/플로이드에 오타가？.py
@@ -0,0 +1,28 @@
+# 플로이드에 오타가?
+import sys
+input = sys.stdin.readline
+
+D = [[0] * 100 for _ in range(100)]
+E = [[0] * 100 for _ in range(100)]
+
+for i in range(99):
+    for j in range(100):
+        if i == j:
+            continue
+
+        if j == 9999:
+            D[i][j] = 1
+            E[i][j] = 1
+            continue
+        
+        D[i][j] = 10000
+        E[i][j] = 10000
+
+for i in range(99):
+    D[i][j] = 1
+    E[i][j] = 1
+
+print(100)
+
+for i in range(100):
+    print(*E[i])

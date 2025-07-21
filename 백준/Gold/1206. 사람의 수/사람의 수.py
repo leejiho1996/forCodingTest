bda4c4fee233dd601a55b061c0a68b705a2c1c15
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-07-22 03:26:34 +0900

    [Gold V] Title: 사람의 수, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1206. 사람의 수/사람의 수.py
+++ 백준/Gold/1206. 사람의 수/사람의 수.py
@@ -11,26 +11,18 @@ for i in range(N):
 
 for i in range(1, 1001):
     cnt = 0
+
     for j in range(N):
-        cur = scores[j]
+        total = scores[j] * i
 
-        start = 1
-        end = i * 10
-
-        while start <= end:
-
-            mid = (start + end) // 2
-
-            if int(mid / i * 1000) <= cur:
-                start = mid + 1
-            else:
-                end = mid - 1
-
-        if int((start - 1) / i * 1000) == cur:
+        if total % 1000 != 0:
+            total = (total // 1000+1) * 1000
+        
+        if total // i == scores[j]:
             cnt += 1
 
     if cnt == N:
         break
-
+        
 print(i)
             

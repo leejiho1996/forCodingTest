f4608909ca3745ac339cf737784c4a9ca32eb6a3
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-07-23 23:09:27 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -8,12 +8,7 @@ for i in range(N):
     start, to = map(int,input().split())
 
     to = to - start
-    start = 0
-    
-    if to - start <= 3:
-        print(to - start)
-        continue
-
+   
     speed = int(to**0.5)
     rest = to - speed**2
     

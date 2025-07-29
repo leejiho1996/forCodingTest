2348364d1090ccd130968b3a2d6ca909a7b1a571
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-07-29 16:34:11 +0900

    [Platinum V] Title: 로스팅하는 엠마도 바리스타입니다, Time: 984 ms, Memory: 200900 KB -BaekjoonHub

--- 백준/Platinum/15647. 로스팅하는 엠마도 바리스타입니다/로스팅하는 엠마도 바리스타입니다.py
+++ 백준/Platinum/15647. 로스팅하는 엠마도 바리스타입니다/로스팅하는 엠마도 바리스타입니다.py
@@ -1,50 +1,55 @@
-# 로스팅 하는 엠마도 바리스타 입니다
+# 로스팅하는 엠마도 바리스타입니다
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(300002)
 
-def calRoot(root, prev, cur, dist):
-
-    total[root] += dist
-    ret = 0
-    
-    for i, d in graph[cur]:
-        if i == prev:
-            continue
-
-        ret += 1
-        ret += calRoot(root, cur, i, dist+d)
-
-    childs[cur] = ret
-        
-    return ret
-    
-def calDist(prev, cur, dist):
-
-    shorter = childs[cur] * dist
-    longer = (N - childs[cur] - 2) * dist
-
-    total[cur] += total[prev] + longer - shorter
-
-    for i, d in graph[cur]:
-        if i == prev:
-            continue
-
-        calDist(cur, i, d)
-        
 N = int(input())
 total = [0] * (N+1)
 childs = [0] * (N+1)
 graph = [[] for _ in range(N+1)]
 
-for i in range(N-1):
+for _ in range(N-1):
     n1, n2, dist = map(int,input().split())
-
     graph[n1].append((n2, dist))
     graph[n2].append((n1, dist))
 
-calRoot(1, -1, 1, 0)
-calDist(-1, 1, 0)
+def calRoot_iterative(start):
+    stack = [(start, -1, 0)]  
+    post = []
+
+    while stack:
+        cur, parent, dist = stack.pop()
+        post.append((cur, parent, dist))
+        for neighbor, d in graph[cur]:
+            if neighbor == parent:
+                continue
+            stack.append((neighbor, cur, d))
+
+    for cur, parent, dist in reversed(post):
+        cnt = 0
+        for neighbor, d in graph[cur]:
+            if neighbor == parent:
+                continue
+            cnt += childs[neighbor] + 1
+            total[start] += total[neighbor] + (childs[neighbor] + 1) * d
+        childs[cur] = cnt
+
+def calDist_iterative(start):
+    stack = [(start, -1, 0)]
+    while stack:
+        cur, parent, dist = stack.pop()
+
+        if parent != -1:
+            shorter = childs[cur] * dist
+            longer = (N - childs[cur] - 2) * dist
+            total[cur] = total[parent] + longer - shorter
+
+        for neighbor, d in graph[cur]:
+            if neighbor == parent:
+                continue
+            stack.append((neighbor, cur, d))
+
+calRoot_iterative(1)
+calDist_iterative(1)
 
 for i in range(1, N+1):
     print(total[i])

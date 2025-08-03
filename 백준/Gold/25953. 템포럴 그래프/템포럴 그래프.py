0c1868b0a3f699fbdee5820bf685a2efdd2267f8
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-08-03 15:36:24 +0900

    [Gold III] Title: 템포럴 그래프, Time: 3244 ms, Memory: 114888 KB -BaekjoonHub

--- 백준/Gold/25953. 템포럴 그래프/템포럴 그래프.py
+++ 백준/Gold/25953. 템포럴 그래프/템포럴 그래프.py
@@ -6,27 +6,19 @@ N, T, M = map(int,input().split())
 S, E = map(int,input().split())
 MAX = 10_000_001
 
-# 템포럴 그래프 정의
-graph = [[[] for _ in range(N)] for _ in range(T+1)]
-
 # dp
 dp = [[MAX] * N for _ in range(T+1)]
-
-for i in range(1, T+1):
-    for j in range(M):
-        n1, n2, w = map(int,input().split())
-        graph[i][n1].append((n2, w))
-        graph[i][n2].append((n1, w))
-
 dp[0][S] = 0
 
-for t in range(1, T+1):
-    for n1 in range(N):
-        dp[t][n1] = dp[t-1][n1]
-
-        for n2, w in graph[t][n1]:
-            dp[t][n1] = min(dp[t][n1], dp[t-1][n2] + w)
-
+for i in range(1, T+1):
+    for j in range(N):
+        dp[i][j] = dp[i-1][j]
+        
+    for j in range(M):
+        n1, n2, w = map(int,input().split())
+        dp[i][n1] = min(dp[i][n1], dp[i-1][n2] + w)
+        dp[i][n2] = min(dp[i][n2], dp[i-1][n1] + w)
+        
 if dp[T][E] == MAX:
     print(-1)
 else:

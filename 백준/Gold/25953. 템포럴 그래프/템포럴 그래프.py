4fd8a43443e715252898dc6f1b50b11eb3673d07
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-08-04 00:32:23 +0900

    [Gold III] Title: 템포럴 그래프, Time: 1400 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/25953. 템포럴 그래프/템포럴 그래프.py
+++ 백준/Gold/25953. 템포럴 그래프/템포럴 그래프.py
@@ -7,19 +7,18 @@ S, E = map(int,input().split())
 MAX = 10_000_001
 
 # dp
-dp = [[MAX] * N for _ in range(T+1)]
-dp[0][S] = 0
+dp = [MAX] * N
+dp[S] = 0
 
 for i in range(1, T+1):
-    for j in range(N):
-        dp[i][j] = dp[i-1][j]
+    prev = dp.copy()
         
     for j in range(M):
         n1, n2, w = map(int,input().split())
-        dp[i][n1] = min(dp[i][n1], dp[i-1][n2] + w)
-        dp[i][n2] = min(dp[i][n2], dp[i-1][n1] + w)
+        dp[n1] = min(dp[n1], prev[n2] + w)
+        dp[n2] = min(dp[n2], prev[n1] + w)
         
-if dp[T][E] == MAX:
+if dp[E] == MAX:
     print(-1)
 else:
-    print(dp[T][E])
+    print(dp[E])

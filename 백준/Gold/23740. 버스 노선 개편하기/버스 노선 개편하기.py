f4cb43937fbff2cfc8b2412a5988382c4395e8e1
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-08-08 14:01:36 +0900

    [Gold V] Title: 버스 노선 개편하기, Time: 628 ms, Memory: 80640 KB -BaekjoonHub

--- 백준/Gold/23740. 버스 노선 개편하기/버스 노선 개편하기.py
+++ 백준/Gold/23740. 버스 노선 개편하기/버스 노선 개편하기.py
@@ -10,20 +10,21 @@ for i in range(N):
     S, E, C = map(int,input().split())
     routes.append((S, E, C))
 
-routes.sort()
+routes.sort() # 노선들을 오름차순으로 정렬
 
 head, tail, cost = routes[0]
 
 for i in range(1, N):
     s, e, c = routes[i]
     
-    if s <= tail:
-        tail = max(tail, e)
-        cost = min(cost, c)
+    if s <= tail: # 노선이 겹치는 경우
+        tail = max(tail, e) # 끝나는 부분이 더 먼 것으로 갱신
+        cost = min(cost, c) # 비용은 최소로 갱신
     else:
-        result.append([head, tail, cost])
+        result.append((head, tail, cost))
         head, tail, cost = s, e, c
 
+# 마지막 노선도 넣어줘야한다.
 result.append((head, tail, cost))
 
 print(len(result))

fe78671c22daf845dcbed3faa93e49781ed2e790
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-08-10 17:02:23 +0900

    [Gold III] Title: Min Max Mex, Time: 904 ms, Memory: 73520 KB -BaekjoonHub

--- 백준/Gold/33714. Min Max Mex/Min Max Mex.java
+++ 백준/Gold/33714. Min Max Mex/Min Max Mex.java
@@ -23,46 +23,47 @@ public class Main {
                 map.put(num, 1);
             }
         }
-
-        nums.sort(Comparator.naturalOrder());
-
+        
         HashSet<Integer> set = new HashSet<>(nums);
         ArrayList<Integer> setToList = new ArrayList<>(set);
         setToList.sort(Comparator.naturalOrder());
 
-        int min = 200000;
-        for (int i = 0; i < 200000; i++) {
+        int min = N;
+        for (int i = 0; i < N; i++) {
             if (!map.containsKey(i) || map.get(i) <= K) {
                 min = i;
                 break;
             }
         }
 
-        System.out.println(min);
+        System.out.println(min); // 최소값 출력
 
 
-        int prev = -1;
+        int prev = -1; // 가능한 최솟값이 0이니 초기 prev는 -1
         boolean check = false;
 
         for (int i = 0; i < setToList.size(); i++) {
+            // 이전 값과 현재 값 사이에 넣어야하는 숫자 갯수
             int fill = setToList.get(i) - prev - 1;
 
-            if (fill < 1) {
+            if (fill == 0) { // 채울 필요가 없으면 패스
                 prev = setToList.get(i);
             }
 
+            // K번의 연산으로 채울 수 없다면 break
             if (K - fill < 0) {
                 check = true;
                 break;
-            } else {
+            } else { // 채울 수 있다면 다음 값을 넘어간다
                 K -= fill;
                 prev = setToList.get(i);
             }
         }
 
+        // break 되었다면 이전 값에 (K+1)
         if (check) {
             System.out.println(prev + (K+1));
-        } else {
+        } else { // 아니라면 마지막 값에 K+1
             System.out.println(setToList.get(setToList.size() - 1)+ (K+1));
         }
     }

292e2237a828fc0acaef75baba7f99aba69a1fc1
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-08-22 22:19:32 +0900

    [Gold V] Title: 단어 마방진, Time: 1240 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/24891. 단어 마방진/단어 마방진.py
@@ -0,0 +1,49 @@
+# 단어 마방진
+import sys
+input = sys.stdin.readline
+
+def check(arr):
+
+    for i in range(L):
+        row = arr[i]
+        col = ""
+        for j in range(L):
+            col += arr[j][i]
+
+        if row != col:
+            return False
+
+    return True
+
+def solve(cur, cnt):
+
+    if cnt == L:
+        if check(cur):
+            for i in cur:
+                print(i)
+            exit()
+        return
+
+    for i in range(N):
+        if visited[i]:
+            continue
+
+        visited[i] = 1
+        cur.append(words[i])
+
+        solve(cur, cnt+1)
+
+        cur.pop()
+        visited[i] = 0
+        
+L, N = map(int,input().split())
+words = []
+visited = [0] * N
+
+for i in range(N):
+    words.append(input().rstrip())
+
+words.sort()
+
+solve([], 0)
+print("NONE")

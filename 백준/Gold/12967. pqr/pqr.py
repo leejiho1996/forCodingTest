0d8c4b6b8862d79b8326ede3b4519b3dd629458f
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-09-09 19:16:00 +0900

    [Gold I] Title: pqr, Time: 832 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/12967. pqr/pqr.py
+++ 백준/Gold/12967. pqr/pqr.py
@@ -15,34 +15,39 @@ N, K = map(int,input().split())
 nums = list(map(int,input().split()))
 result = 0
 
-sub = [0] * 1000001
+dic = {}
 
 for i in range(N):
-    divide = 1
-    cur = nums[i]
-    
-    while divide <= int(cur**0.5):
-        if cur % divide == 0:
-            sub[divide] += 1
-            divided = cur // divide
+    y = gcd(nums[i], K)
 
-            if divided != divide and divided <= 1000000:
-                sub[cur//divide] += 1
+    if y in dic:
+        dic[y] += 1
+    else:
+        dic[y] = 1
 
-        divide += 1
+pairs = list(dic.items())
+length = len(pairs)
 
-for i in range(N):
-    n1 = nums[i]
-    for j in range(i+1, N):
-        n2 = nums[j]
-        y = K // gcd(n1*n2, K)
+for i in range(length):
+    ki, vi = pairs[i]
+    for j in range(i, length):
+        kj, vj = pairs[j]
+        for k in range(j, length):
+            kk, vk = pairs[k]
 
-        result += sub[y]
-        
-        if n1 % y == 0:
-            result -= 1
+            if ki * kj * kk % K != 0:
+                continue
 
-        if n2 % y == 0:
-            result -= 1
-            
-print(result // 3)
+            if i == j == k:
+                result += vi * (vi-1) * (vi-2) // 6
+
+            elif i == j:
+                result += vi * (vi-1) * vk // 2
+
+            elif j == k:
+                result += vi * vj * (vk-1) // 2
+
+            else:
+                result += vi * vj * vk
+                 
+print(result)

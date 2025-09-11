75bc9658d2e5bf04e13bcac44f0755157fa97ba8
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-09-11 17:39:50 +0900

    [Silver I] Title: 내려가기 2, Time: 268 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15645. 내려가기 2/내려가기 2.py
+++ 백준/Silver/15645. 내려가기 2/내려가기 2.py
@@ -3,21 +3,18 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-graph = []
-
-for i in range(N):
-    graph.append(list(map(int,input().split())))
-
 min_dp = [[0] * 3 for _ in range(2)]
 max_dp = [[0] * 3 for _ in range(2)]
-
-min_dp[0] = graph[0].copy()
-max_dp[0] = graph[0].copy()
-
 prev = -1
 cur = 0
 
-for i in range(1, N):
+for i in range(N):
+    row = list(map(int,input().split()))
+
+    if i == 0:
+        min_dp[0] = row.copy()
+        max_dp[0] = row.copy()
+        continue
 
     if i % 2:
         prev = 0
@@ -25,13 +22,13 @@ for i in range(1, N):
     else:
         prev = 1
         cur = 0
-        
-    min_dp[cur][0] = graph[i][0] + min(min_dp[prev][0], min_dp[prev][1])
-    min_dp[cur][1] = graph[i][1] + min(min_dp[prev][0], min_dp[prev][1], min_dp[prev][2])
-    min_dp[cur][2] = graph[i][2] + min(min_dp[prev][1], min_dp[prev][2])
 
-    max_dp[cur][0] = graph[i][0] + max(max_dp[prev][0], max_dp[prev][1])
-    max_dp[cur][1] = graph[i][1] + max(max_dp[prev][0], max_dp[prev][1], max_dp[prev][2])
-    max_dp[cur][2] = graph[i][2] + max(max_dp[prev][1], max_dp[prev][2])
+    min_dp[cur][0] = row[0] + min(min_dp[prev][0], min_dp[prev][1])
+    min_dp[cur][1] = row[1] + min(min_dp[prev][0], min_dp[prev][1], min_dp[prev][2])
+    min_dp[cur][2] = row[2] + min(min_dp[prev][1], min_dp[prev][2])
+
+    max_dp[cur][0] = row[0] + max(max_dp[prev][0], max_dp[prev][1])
+    max_dp[cur][1] = row[1] + max(max_dp[prev][0], max_dp[prev][1], max_dp[prev][2])
+    max_dp[cur][2] = row[2] + max(max_dp[prev][1], max_dp[prev][2])
 
 print(max(max_dp[cur]), min(min_dp[cur]))

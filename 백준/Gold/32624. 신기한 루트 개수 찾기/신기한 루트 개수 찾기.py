e123a396cf3adcefac65b8bf39d0270d60176b37
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-09-14 02:12:19 +0900

    [Gold III] Title: 신기한 루트 개수 찾기, Time: 1044 ms, Memory: 120704 KB -BaekjoonHub

--- 백준/Gold/32624. 신기한 루트 개수 찾기/신기한 루트 개수 찾기.py
+++ 백준/Gold/32624. 신기한 루트 개수 찾기/신기한 루트 개수 찾기.py
@@ -14,8 +14,8 @@ def dfs(cur):
         if visited[i]:
             continue
         
-        if i == B-1:
-            child += 1
+        if i == B:
+            child += 1 # B와 이어진 노드라면 체크
             continue
 
         nchild, ncnt = dfs(i)
@@ -26,23 +26,22 @@ def dfs(cur):
     return child, cnt
 
 N, A, B = map(int,input().split())
-visited = [0] * N
-graph = [[] for _ in range(N)]
+visited = [0] * (N+1)
+graph = [[] for _ in range(N+1)]
 
 for i in range(N-1):
     n1, n2 = map(int,input().split())
-    graph[n1-1].append(n2-1)
-    graph[n2-1].append(n1-1)
+    graph[n1].append(n2)
+    graph[n2].append(n1)
 
-visited[A-1] = 1
-
-for i in graph[A-1]:
-    if i == B-1:
+visited[A] = 1
+for i in graph[A]:
+    if i == B: # A와 B가 인접 노드라면 LCA가 A, B 밖에 안되므로 0 출력
         print(0)
         break
     
     child, cnt = dfs(i)
 
-    if child:
+    if child: # 만약 B와 연결된 노드를 탐색했다면 카운트 출력후 종료
         print(cnt)
         break

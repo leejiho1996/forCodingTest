d5b6a405f1bb2dd7248e36b417a8eed150db57f5
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-09-29 00:45:16 +0900

    [Gold IV] Title: 자유 이용권, Time: 76 ms, Memory: 44748 KB -BaekjoonHub

---
+++ 백준/Gold/25635. 자유 이용권/자유 이용권.py
@@ -0,0 +1,15 @@
+# 자유 이용권
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+tickets = list(map(int,input().split()))
+tickets.sort(reverse=True)
+
+total = sum(tickets)
+
+if total - tickets[0] >= tickets[0]:
+    print(total)
+else:
+    print(total - tickets[0] + sum(tickets[1:]) + 1)
+        

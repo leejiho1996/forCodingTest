e5149054762b0581d492d74ab8c90be63d42dda2
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-10-22 03:37:49 +0900

    [Gold II] Title: 증가 수열, Time: 104 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1329. 증가 수열/증가 수열.py
+++ 백준/Gold/1329. 증가 수열/증가 수열.py
@@ -1,47 +1,50 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10000)
 
 S = input().rstrip()
 N = len(S)
-best_last = None
-best_seq = None
+best_last = int(S)+1
+best_seq = []
 
 def seq_better(a, b):
+
     for x, y in zip(a, b):
         xi = int(x); yi = int(y)
+
         if xi != yi:
             return xi > yi
-    return len(a) > len(b)
 
 def dfs(start, seq, prev_int):
+
     global best_last, best_seq
+
     if start == N:
         last_int = prev_int
-        if best_last is None:
+        
+        if last_int < best_last:
             best_last = last_int
             best_seq = seq.copy()
-        else:
-            if last_int < best_last:
-                best_last = last_int
+        elif last_int == best_last:
+            if seq_better(seq, best_seq):
                 best_seq = seq.copy()
-            elif last_int == best_last:
-                if seq_better(seq, best_seq):
-                    best_seq = seq.copy()
         return
+
     for i in range(start+1, N+1):
         part = S[start:i]
         cur = int(part)
-        if prev_int is None or cur > prev_int:
-            if best_last is not None and cur > best_last:
+
+        if cur > prev_int:
+
+            if cur > best_last:
                 break
+
             seq.append(part)
             dfs(i, seq, cur)
             seq.pop()
 
-dfs(0, [], None)
+dfs(0, [], -1)
 
-if best_seq is None:
+if best_seq == -1:
     print(S)
 else:
     print(",".join(best_seq))

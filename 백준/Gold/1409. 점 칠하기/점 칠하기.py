7a7d63ef5dfbf774267a5848e0139f068a085a7a
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-11-06 15:46:12 +0900

    [Gold I] Title: 점 칠하기, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1409. 점 칠하기/점 칠하기.py
@@ -0,0 +1,48 @@
+import sys
+
+input = sys.stdin.readline
+
+ang = [0] * 360
+use = [0] * 360
+
+n = int(input())
+for _ in range(n):
+    x = int(input())
+    ang[x] = 1
+
+max_cnt = 0
+for diff in range(1, 360):
+    cnt = 0
+    use = [0] * 360
+    for start in range(360):
+        if use[start]:
+            continue
+
+        cur = start
+        length = 0
+        lens = []
+
+        while True:
+            if ang[cur]:
+                length += 1
+            else:
+                lens.append(length)
+                length = 0
+
+            use[cur] = 1
+            cur = (cur + diff) % 360
+
+            if cur == start:
+                break
+
+        if not lens:
+            lens.append(length)
+        else:
+            lens[0] += length
+
+        for e in lens:
+            cnt += (e // 2) * 2
+
+    max_cnt = max(max_cnt, cnt)
+
+print(max_cnt)

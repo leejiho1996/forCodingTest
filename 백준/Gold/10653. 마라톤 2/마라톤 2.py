4b1062f83d751c8f180b1b56779d969bd42ea029
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-11-28 16:23:47 +0900

    [Gold III] Title: 마라톤 2, Time: 600 ms, Memory: 113772 KB -BaekjoonHub

--- 백준/Gold/10653. 마라톤 2/마라톤 2.py
+++ 백준/Gold/10653. 마라톤 2/마라톤 2.py
@@ -2,6 +2,9 @@
 import sys
 input = sys.stdin.readline
 
+def cal(p1, p2):
+    return abs(points[p1][0]-points[p2][0]) + abs(points[p1][1]-points[p2][1])
+
 def solve(e, k):
 
     if e == 0:
@@ -19,21 +22,16 @@ def solve(e, k):
         
         dp[e][k] = min(
             dp[e][k],
-            solve(e-i-1, k-i) +
-            abs(points[e][0] - points[e-i-1][0]) + abs(points[e][1] - points[e-i-1][1]))
+            solve(e-i-1, k-i) + cal(e, e-i-1))
 
     return dp[e][k]
 
 N, K = map(int,input().split())
 points = []
 dp = [[-1] * (K+1) for _ in range(N)]
-dist = [0] * (N+1)
 
 for i in range(N):
     x, y = map(int,input().split())
     points.append((x, y))
-
-    if i != 0:
-        dist[i] = dist[i-1] + abs(points[i-1][0] - x) + abs(points[i-1][1] - y)
 
 print(solve(N-1, K))

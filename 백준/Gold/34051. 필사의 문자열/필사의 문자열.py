eb51a2fa574318c06b670e03c2aad3aa625dfb0b
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-12-09 16:49:57 +0900

    [Gold IV] Title: 필사의 문자열, Time: 1624 ms, Memory: 119192 KB -BaekjoonHub

---
+++ 백준/Gold/34051. 필사의 문자열/필사의 문자열.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+W = input().rstrip()
+
+minn = [5001] * N
+candi = []
+
+for i in range(N):
+    for j in range(i+1, N):
+        if W[i] < W[j] and minn[j] == 5001:
+            minn[j] = i
+            candi.append((i, j))
+
+result = W
+
+for i, j in candi:
+    tmp = W[:i]
+    
+    for k in range(j, i-1, -1):
+        tmp += W[k]
+   
+    tmp += W[j+1:]
+
+    if tmp > result:
+        result = tmp
+
+print(result)
+

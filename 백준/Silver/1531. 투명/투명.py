c4ec766d1e1cb2565c4480d1d0697916abb8b4e5
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-12-12 10:26:38 +0900

    [Silver V] Title: 투명, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1531. 투명/투명.py
@@ -0,0 +1,22 @@
+# 투명
+import sys
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+graph = [[0] * 100 for _ in range(100)]
+
+for i in range(N):
+    x1, y1, x2, y2 = map(int,input().split())
+
+    for i in range(x1-1, x2):
+        for j in range(y1-1, y2):
+            graph[i][j] += 1
+
+cnt = 0
+
+for i in range(100):
+    for j in range(100):
+        if graph[i][j] > M:
+            cnt += 1
+
+print(cnt)

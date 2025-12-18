91a413b8bd9de27e30c298aaaa5a867dafa9dafd
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-12-18 22:12:54 +0900

    [Bronze IV] Title: 특별한 날, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+
+if a > 2:
+    print("After")
+elif a == 2 and b > 18:
+    print("After")
+elif a == 2 and b == 18:
+    print("Special")
+else:
+    print("Before")

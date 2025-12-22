e08b05ec6c391500794f915a681d549dafdea25e
leejiho1996 <136657786+leejiho1996@users.noreply.github.com>
2025-12-22 18:27:18 +0900

    [Gold IV] Title: 작업, Time: 432 ms, Memory: 38516 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.py
+++ 백준/Gold/2056. 작업/작업.py
@@ -3,11 +3,14 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-front = [0] * N
 time = [0] * N
 res = [0] * N
+front = [0] * N
 tail = [[] for _ in range(N)]
+
 stack = []
+
+mx = 0
 
 for i in range(N):
     tmp = list(map(int,input().split()))
@@ -15,16 +18,17 @@ for i in range(N):
     time[i] = tmp[0]
     front[i] = tmp[1]
 
-    for j in range(tmp[1]):
+    for j in range(tmp[1]): # 선행작업들의 리스트에 추가
         tail[tmp[2+j]-1].append(i)
 
-    if tmp[1] == 0:
+    if tmp[1] == 0: # 선행작업이 없다면 스택에 추가
         stack.append((i, tmp[0]))
         res[i] = tmp[0]
 
 while stack:
 
-    cur, ct = stack.pop()
+    cur, ct = stack.pop() # 끝난 작업과 끝난 시간
+    mx = max(mx, ct)
 
     for j in (tail[cur]):
         front[j] -= 1
@@ -34,4 +38,4 @@ while stack:
         if front[j] == 0:
             stack.append((j, res[j]))
 
-print(max(res))
+print(mx)
